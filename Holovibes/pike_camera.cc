ddf03bd2ca9e940a9612a3eb0db0bc07710cae49
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-09-18 15:41:04 +0200

    almost finished initializing of camera

--- Holovibes/pike_camera.cc
+++ Holovibes/pike_camera.cc
@@ -1,17 +1,39 @@
 #include "pike_camera.hh"
 
 #define MAXNAMELENGTH 256
+#define MAXCAMERAS 1
 
 namespace camera
 {
 
   bool PikeCamera::init_camera()
   {
-    if (cam_.StartDevice() != 0)
+    unsigned long result;
+    FGNODEINFO* nodes_info;
+    unsigned long max_nodes = MAXCAMERAS;
+    unsigned long copied_nodes = 0;
+
+    //Prepare the entire library for use
+    result = FGInitModule(NULL);
+
+    //FCE_NOERROR = 0
+    if (result == FCE_NOERROR)
     {
-      return false;
+      //Retrieve list of connected nodes (cameras)
+      //Ask for a maximum number of nodes info to fill (max_nodes)
+      //and put them intos nodes_info. It also puts the number of nodes
+      //effectively copied.
+      result = FGGetNodeList(nodes_info, max_nodes, &copied_nodes);
     }
 
+    //If there is no errors and at least one node detected.
+    if (result == FCE_NOERROR && copied_nodes)
+    {
+      //FIXME
+      //Connect first node with our cam_ object
+    }
+
+    //Retrieve name from device and fill name_ with it
     name_ = get_name_from_device();
 
     return true;
@@ -26,6 +48,11 @@ namespace camera
 
   }
 
+  void PikeCamera::shutdown_camera()
+  {
+
+  }
+
   std::string PikeCamera::get_name_from_device()
   {
     char* ccam_name = new char[MAXNAMELENGTH];
@@ -36,10 +63,5 @@ namespace camera
     std::string cam_name(ccam_name);
 
     return cam_name;
-  }
-
-  void PikeCamera::shutdown_camera()
-  {
-
   }
 }
