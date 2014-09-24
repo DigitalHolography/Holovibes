abc4a507d2b44c0f432d065ff0c2d190a4d76dab
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-09-24 10:45:36 +0200

    add Frame class

--- Holovibes/camera.hh
+++ Holovibes/camera.hh
@@ -2,18 +2,14 @@
 # define CAMERA_HH
 
 # include <string>
+# include "endianness.hh"
+# include "frame.hh"
 
 namespace camera
 {
   class Camera
   {
   public:
-    enum endianness
-    {
-      BIG_ENDIAN,
-      LITTLE_ENDIAN
-    };
-
     Camera(std::string name)
       : name_(name)
     {}
@@ -32,33 +28,20 @@ namespace camera
       return endianness_;
     }
 
-    int get_frame_width() const
-    {
-      return frame_width_;
-    }
-
-    int get_frame_height() const
-    {
-      return frame_height_;
-    }
-
     // Virtual methods
     virtual bool init_camera() = 0;
     virtual void start_acquisition() = 0;
     virtual void stop_acquisition() = 0;
     virtual void shutdown_camera() = 0;
 
-    virtual void* get_frame() = 0;
+    virtual Frame& get_frame() = 0;
 
   protected:
     std::string name_;
 
   private:
     enum endianness endianness_;
-
-    int frame_height_;
-    int frame_width_;
-    unsigned char frame_bit_depth_;
+    Frame frame_;
   };
 }
 
---
+++ Holovibes/endianness.hh
@@ -0,0 +1,13 @@
+#ifndef ENDIANNESS_HH
+# define ENDIANNESS_HH
+
+namespace camera
+{
+  typedef enum endianness
+  {
+    BIG_ENDIAN,
+    LITTLE_ENDIAN
+  } e_endianness;
+}
+
+#endif /* !ENDIANNESS_HH */
