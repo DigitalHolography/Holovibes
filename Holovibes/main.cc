bf1575bc51d73295eb1db9096aa19e351ef4e5b9
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-09-26 13:10:53 +0200

    add main dor pike camera

--- Holovibes/main.cc
+++ Holovibes/main.cc
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include "pike_camera.hh"
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
+  camera::PikeCamera cam("");
+  bool is_init = cam.init_camera();
+
+  if (is_init)
+  {
+    std::cout << cam.get_name() << std::endl;
+    cam.start_acquisition();
+
+    FGFRAME* frame = (FGFRAME*) cam.get_frame();
+
+    // Writting to file
+    if (frame != nullptr)
+    {
+      std::ofstream stream;
+      stream.open("test.raw");
+      if (frame->pData)
+        stream.write((char*)frame->pData, frame->Length);
+      else
+        std::cout << "frame pdata null" << std::endl;
+      stream.close();
+    }
+
+    cam.stop_acquisition();
+    cam.shutdown_camera();
+  }
+  else
+    std::cout << "Camera not initialized" << std::endl;
+
+  getchar();
   return 0;
 }
