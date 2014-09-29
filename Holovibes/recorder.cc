c251d7b8e8d79f6d1358741319b832f9f73c5cca
Pierre PAGNOUX <pierre.pagnoux@espci.fr>
2014-09-29 10:41:57 +0200

    retrieve recorder's changes from new/develop

--- Holovibes/recorder.cc
+++ Holovibes/recorder.cc
@@ -4,19 +4,19 @@ namespace recorder
 {
   Recorder::Recorder(queue::Queue *queue, std::string path, int set_size)
   {
-    //if (buffer_ && buffer_->get_max_elts() > 
-    set_size_ = set_size;
+    if (queue->get_max_elts() <= set_size)
+      set_size_ = 1;
+    else
+      set_size_ = set_size;
     buffer_ = queue;
     fd_ = fopen(path.c_str(), "w+");
   }
 
   void Recorder::record()
   {
-    int written = 0;
     if (buffer_->get_current_elts() >= set_size_) // we record them to the drive
     {
-      written = contigous_image();
-      std::cout << "written" << written << std::endl;
+      int written = contigous_image();
       fwrite(buffer_->dequeue(written), buffer_->get_size(), written, fd_);
     }
   }
@@ -26,7 +26,6 @@ namespace recorder
       return buffer_->get_current_elts();
     else
     {
-      std::cout << "BOOOM" << std::endl;
       return buffer_->get_max_elts() - buffer_->get_start_index();
     }
       
