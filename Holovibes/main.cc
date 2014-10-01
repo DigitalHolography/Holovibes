1d99813c42363a7caf7944ccab8c3af2fa90b7b7
Thomas KOSTAS <thomas.kostas@espci.fr>
2014-10-01 13:31:42 +0200

    Added main.hh

--- Holovibes/main.cc
+++ Holovibes/main.cc
@@ -1,16 +1,19 @@
 #include "stdafx.h"
+#include "main.hh"
 #include <iostream>
 #include <fstream>
 #include <cstdio>
 #include "pike_camera.hh"
 #include "queue.hh"
-#include "main.hh"
+
 
 using namespace holovibes;
+using namespace camera;
 
 int main(const int argc, const char *argv[])
 {
   OptionParser *op = gen_opt_parser(argc, argv);
+  
   std::cout << "Hello World!" << std::endl;
 
   return 0;
@@ -22,23 +25,29 @@ OptionParser *gen_opt_parser(const int argc, const char *argv[])
   op->init_parser();
   op->proceed();
   return op;
-
 }
 
 void manage_parsed_info(s_options opt)
 {
   if (opt.display_images)
   {
-    draw_in_win(launch_display(opt));
+    //draw_in_win(launch_display(opt));
+  }
+  else
+  {
   }
 
 }
 
-
-
-void draw_in_win(GLWindow *win)
+Camera *create_cam()
 {
+  return (new PikeCamera());
+}
 
+void draw_in_win(GLWindow *win, Camera *cam, s_options opt)
+{
+  for (int i = 0; i < opt.nbimages; i++)
+    win->gl_draw(cam->get_frame(), opt.width, opt.height);
 }
 
 GLWindow *launch_display(s_options opt)
