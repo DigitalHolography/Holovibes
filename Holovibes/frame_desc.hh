f4a4b5c2be4a4127483444467f852b26be5d0a09
Pierre PAGNOUX <pierre.pagnoux@espci.fr>
2014-10-03 11:47:44 +0200

    update camera interface

--- Holovibes/frame_desc.hh
+++ Holovibes/frame_desc.hh
@@ -11,14 +11,22 @@ namespace camera
     LITTLE_ENDIAN
   } e_endianness;
 
+  /*! This structure contains everything related to the image format. */
   typedef struct frame_descriptor
   {
+    /*! Width of the frame. */
     unsigned int         width;
+    /*! Height of the frame. */
     unsigned int         height;
+    /*! Bit depth during acquisition. */
     unsigned char        bit_depth;
+    /*! Size of pixels in micrometers. */
+    float                pixel_size;
+    /*! Endianness of bytes. */
     e_endianness         endianness;
 
   public:
+    /* Helper functions. */
     unsigned char get_byte_depth() const
     {
       return bit_depth / 8 + (bit_depth % 8 ? 1u : 0u);
