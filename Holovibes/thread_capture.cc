49c8a80763ca67ec6019044df11011cb9997e1d1
Pierre PAGNOUX <pierre.pagnoux@espci.fr>
2014-10-15 10:35:47 +0200

    add .cc file for thread_capture class

---
+++ Holovibes/thread_capture.cc
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "thread_capture.hh"
+
+namespace holovibes
+{
+  ThreadCapture::ThreadCapture(camera::Camera& camera, Queue& queue)
+    : camera_(camera)
+    , queue_(queue)
+    , stop_requested_(false)
+    , thread_(&ThreadCapture::thread_proc, this)
+  {}
+
+  ThreadCapture::~ThreadCapture()
+  {
+    stop_requested_ = true;
+    if (thread_.joinable())
+      thread_.join();
+  }
+
+  void ThreadCapture::thread_proc()
+  {
+    while (!stop_requested_)
+      queue_.enqueue(camera_.get_frame());
+  }
+}
--- Holovibes/thread_capture.hh
+++ Holovibes/thread_capture.hh
@@ -2,6 +2,7 @@
 # define THREAD_CAPTURE_HH
 
 # include <thread>
+
 # include "camera.hh"
 # include "queue.hh"
 
@@ -10,31 +11,17 @@ namespace holovibes
   class ThreadCapture
   {
   public:
-    ThreadCapture(camera::Camera& cam, Queue& q)
-      : cam_(cam),
-      q_(q),
-      th_(&ThreadCapture::capture, this),
-      is_on_(true)
-    {
-    }
-
-    ~ThreadCapture()
-    {
-      is_on_ = false;
-      th_.join();
-    }
+    ThreadCapture(camera::Camera& camera, Queue& queue);
+    ~ThreadCapture();
 
   private:
-    camera::Camera& cam_;
-    Queue& q_;
-    std::thread th_;
-    bool is_on_;
+    void thread_proc();
 
-    void capture()
-    {
-      while (is_on_)
-        q_.enqueue(cam_.get_frame());
-    }
+  private:
+    camera::Camera& camera_;
+    Queue& queue_;
+    bool stop_requested_;
+    std::thread thread_;
   };
 }
 
