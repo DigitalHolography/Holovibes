90ef8e783ed195ed355f30b19b64b46de5cb4feb
Thomas KOSTAS <thomas.kostas@espci.fr>
2014-11-12 15:04:32 +0100

    Contrast correction auto and manual

--- Holovibes/contrast_correction.cuh
+++ Holovibes/contrast_correction.cuh
@@ -15,7 +15,8 @@
 #include "hardware_limits.hh"
 
 
-void correct_contrast(unsigned char *img, int img_size, int bytedepth);
+void manual_contrast_correction(void *img, unsigned int img_size, int bytedepth, unsigned int manual_min, unsigned int manual_max);
+void auto_contrast_correction(unsigned int *min, unsigned int *max, void *img, unsigned int img_size, unsigned int bytedepth, unsigned int percent);
 void sum_histo(int *histo, int *summed_histo);
 
 
