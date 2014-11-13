f631003a6a46a4045076e7aa5ee35985a64f6b0d
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-11-13 16:51:41 +0100

    slots for most widgets of main window

--- Holovibes/main_window.cc
+++ Holovibes/main_window.cc
@@ -12,4 +12,85 @@ namespace gui
   MainWindow::~MainWindow()
   {
   }
+
+  void MainWindow::set_image_mode(bool value)
+  {
+    print_parameter("image mode", value);
+  }
+
+  void  MainWindow::set_phase_number(int value)
+  {
+    print_parameter("phase number", value);
+  }
+
+  void  MainWindow::set_p(int value)
+  {
+    print_parameter("p", value);
+  }
+
+  void  MainWindow::set_wavelength(double value)
+  {
+    print_parameter("wavelength", value);
+  }
+
+  void  MainWindow::set_z(double value)
+  {
+    print_parameter("z", value);
+  }
+
+  void  MainWindow::set_algorithm(QString value)
+  {
+    print_parameter("p", qPrintable(value));
+  }
+
+  void MainWindow::set_auto_contrast()
+  {
+    print_parameter("auto contrast", "enabled");
+  }
+
+  void MainWindow::set_contrast_min(double value)
+  {
+    print_parameter("contrast min", value);
+  }
+
+  void MainWindow::set_contrast_max(double value)
+  {
+    print_parameter("contrast max", value);
+  }
+
+  void MainWindow::set_log_scale(bool value)
+  {
+    print_parameter("log scale", value);
+  }
+
+  void MainWindow::set_shifted_corners(bool value)
+  {
+    print_parameter("shifted corners", value);
+  }
+
+  void MainWindow::set_p_vibro(int value)
+  {
+    print_parameter("p vibrometry", value);
+  }
+
+  void MainWindow::set_q_vibro(int value)
+  {
+    print_parameter("q vibrometry", value);
+  }
+
+  void MainWindow::set_number_of_frames(int value)
+  {
+    print_parameter("number of frames", value);
+  }
+
+  void MainWindow::set_record()
+  {
+    print_parameter("record", "enabled");
+  }
+
+  template <typename T>
+  void MainWindow::print_parameter(std::string name, T value)
+  {
+    std::cout << "Parameter " << name << " changed to " << std::boolalpha << value << std::endl;
+  }
 }
--- Holovibes/main_window.hh
+++ Holovibes/main_window.hh
@@ -14,8 +14,36 @@ namespace gui
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+  public slots:
+    // Image rendering
+    void set_image_mode(bool value);
+    void set_phase_number(int value);
+    void set_p(int value);
+    void set_wavelength(double value);
+    void set_z(double value);
+    void set_algorithm(QString value);
+
+    // View
+    void set_auto_contrast();
+    void set_contrast_min(double value);
+    void set_contrast_max(double value);
+    void set_log_scale(bool value);
+    void set_shifted_corners(bool value);
+
+    // Special
+    void set_p_vibro(int value);
+    void set_q_vibro(int value);
+
+    // Record
+    void set_number_of_frames(int value);
+    void set_record();
+
   private:
     Ui::MainWindow ui;
+
+    //Debug
+    template <typename T>
+    void print_parameter(std::string name, T value);
   };
 }
 
