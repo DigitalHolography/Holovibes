4e219eec847eb1679051884f2c5bbb7c3a232e05
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-11-14 13:56:15 +0100

    calling argument for tests

--- Holovibes/tools.cuh
+++ Holovibes/tools.cuh
@@ -14,6 +14,8 @@
 __global__ void image_2_complex8(cufftComplex* res, unsigned char* data, int size, float *sqrt_tab);
 __global__ void image_2_complex16(cufftComplex* res, unsigned short* data, int size, float *sqrt_tab);
 __global__ void complex_2_module(cufftComplex* input, unsigned short* output, int size);
+__global__ void complex_2_squared_magnitude(cufftComplex* input, unsigned short* output, int size);
+__global__ void complex_2_argument(cufftComplex* input, unsigned short* output, int size);
 __global__ void apply_quadratic_lens(cufftComplex *input, int input_size, cufftComplex *lens, int lens_size);
 __global__ void meshgrind_square(float *input_u, float *input_v, float *output_u, float *output_v, unsigned int size_x, unsigned int size_y);
 __global__ void fft2_make_u_v(float pasu, float pasv, float *u, float *v, unsigned int size_x, unsigned int size_y);
