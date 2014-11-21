76e10b8b11d0ad1165e6239b1a5a6641d7476236
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2014-11-21 17:12:06 +0100

    Hardware limit function memoization

--- Holovibes/hardware_limits.cc
+++ Holovibes/hardware_limits.cc
@@ -6,21 +6,48 @@
 
 unsigned int get_max_threads_1d()
 {
-  cudaDeviceProp prop;
-  cudaGetDeviceProperties(&prop, 0);
-  return prop.maxThreadsPerBlock;
+  static int max_threads_per_block_1d;
+  static bool initialized = false;
+
+  if (!initialized)
+  {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    max_threads_per_block_1d = prop.maxThreadsPerBlock;
+    initialized = true;
+  }
+
+  return max_threads_per_block_1d;
 }
 
 unsigned int get_max_threads_2d()
 {
-  cudaDeviceProp prop;
-  cudaGetDeviceProperties(&prop, 0);
-  return static_cast<unsigned int>(sqrt(prop.maxThreadsPerBlock));
+  static int max_threads_per_block_2d;
+  static bool initialized = false;
+
+  if (!initialized)
+  {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    max_threads_per_block_2d = static_cast<unsigned int>(sqrt(prop.maxThreadsPerBlock));
+    initialized = true;
+  }
+
+  return max_threads_per_block_2d;
 }
 
 unsigned int get_max_blocks()
 {
-  cudaDeviceProp prop;
-  cudaGetDeviceProperties(&prop, 0);
-  return prop.maxGridSize[0];
+  static int max_blocks;
+  static bool initialized = false;
+
+  if (!initialized)
+  {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    max_blocks = prop.maxGridSize[0];
+    initialized = true;
+  }
+
+  return max_blocks;
 }
