d53e543f8001d249f90e333161bf25a24bb7e120
Jeffrey BENCTEUX <jeffrey.bencteux@espci.fr>
2015-01-14 10:51:11 +0100

    GroupBox class documentation

--- Holovibes/gui_group_box.hh
+++ Holovibes/gui_group_box.hh
@@ -5,15 +5,24 @@
 
 namespace gui
 {
+  /*! \class GroupBox
+  **
+  ** QGroupBox overload, used to hide and show parts of the GUI.
+  */
   class GroupBox : public QGroupBox
   {
     Q_OBJECT
 
   public:
+    /*! \brief GroupBox constructor
+    ** \param parent Qt parent 
+    */
     GroupBox(QWidget* parent = 0);
+    /*! \brief GroupBox destructor */
     ~GroupBox();
 
   public slots:
+    /*! \brief Show or hide GroupBox */
     void ShowOrHide();
   };
 }
