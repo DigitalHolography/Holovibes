576da52404a8d73eef8f0105ecbaba187af3f0b2
Eric <9ryu108@gmail.com>
2015-09-25 17:47:59 +0200

    add recursive create dir and resize main_window.ui

--- Holovibes/recorder.cc
+++ Holovibes/recorder.cc
@@ -4,9 +4,52 @@
 #include <cassert>
 #include <thread>
 #include <iostream>
+#include <list>
+#include <boost/filesystem.hpp>
+#include <direct.h>
 
 namespace holovibes
 {
+	void	createFilePath(const std::string folderName)
+	{
+		std::list<std::string> folderLevels;
+		std::string startPath = boost::filesystem::current_path().string();
+		char* c_str = (char*)folderName.c_str();
+
+		// Point to end of the string
+		char* strPtr = &c_str[strlen(c_str) - 1];
+
+		// Create a list of the folders which do not currently exist
+		do {
+			if (boost::filesystem::exists(c_str)) {
+				break;
+			}
+			// Break off the last folder name, store in folderLevels list
+			do {
+				strPtr--;
+			} while ((*strPtr != '\\') && (*strPtr != '/') && (strPtr >= c_str));
+			folderLevels.push_front(std::string(strPtr + 1));
+			strPtr[1] = 0;
+		} while (strPtr >= c_str);
+
+		if (folderLevels.empty())
+			return;
+		std::cout << folderLevels.back() << std::endl;
+		folderLevels.pop_back();
+
+		if (_chdir(c_str)) {
+			throw std::exception("[RECORDER] error cannot _chdir directory");
+		}
+		// Create the folders iteratively
+		for (std::list<std::string>::iterator it = folderLevels.begin(); it != folderLevels.end(); it++) {
+			if (boost::filesystem::create_directory(it->c_str()) == 0) {
+				throw std::exception("[RECORDER] error cannot create directory");
+			}
+			_chdir(it->c_str());
+		}
+		_chdir(startPath.c_str());
+	}
+
   Recorder::Recorder(
     Queue& queue,
     const std::string& filepath)
@@ -14,6 +57,7 @@ namespace holovibes
     , file_()
     , stop_requested_(false)
   {
+	  createFilePath(filepath);
 #ifndef _DEBUG
     if (is_file_exist(filepath))
       throw std::exception("[RECORDER] error overwriting an existing file");
