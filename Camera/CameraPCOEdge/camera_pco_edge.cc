69c2b9b5cd791b8cc335d9cdb55e33e050ca4f0f
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-09-28 18:11:07 +0200

    Began extension of camera Edge's configuration possibilities.

--- Camera/CameraPCOEdge/camera_pco_edge.cc
+++ Camera/CameraPCOEdge/camera_pco_edge.cc
@@ -24,10 +24,58 @@ namespace camera
   {
   }
 
+  static void initialize_pco_signal(PCO_Signal& sig, WORD index)
+  {
+    sig.wSize = sizeof(PCO_Signal);
+    sig.wSignalNum = index; // { 0, 1, 2, 3}
+    sig.wEnabled = 1; // on (activated)
+    sig.wType = 1; // signal type : TTL
+    sig.wPolarity = 1; // low level active
+    sig.wFilterSetting = 1; // No signal filter
+    sig.wSelected = 0; // standard signal
+
+    /* TODO : I did NOT understand those! */
+    sig.dwParameter[0] = 0;
+    sig.dwParameter[1] = 0;
+    sig.dwParameter[2] = 0;
+    sig.dwParameter[3] = 0;
+
+    sig.dwSignalFunctionality[0] = 0;
+    sig.dwSignalFunctionality[1] = 0;
+    sig.dwSignalFunctionality[2] = 0;
+    sig.dwSignalFunctionality[3] = 0;
+  }
+
   void CameraPCOEdge::load_default_params()
   {
+    /* Various camera parameters. */
     exposure_time_ = 0.024f;
+
     triggermode_ = 0;
+
+    framerate_ = 30 * 10e3;
+    framerate_mode_ = 1;
+
+    hz_binning_ = 1;
+    vt_binning_ = 1;
+
+    p0_x_ = 0;
+    p0_y_ = 0;
+    p1_x_ = CameraPCO::get_actual_res_x();
+    p1_y_ = CameraPCO::get_actual_res_y();
+
+    pixel_rate_ = 12 * 10e6;
+
+    conversion_factor_ = 460;
+
+    ad_converters_ = 1;
+
+    timeouts_[0] = timeouts_[1] = timeouts_[2] =  50;
+
+    initialize_pco_signal(io_1_conf, 0);
+    initialize_pco_signal(io_2_conf, 1);
+    initialize_pco_signal(io_3_conf, 2);
+    initialize_pco_signal(io_4_conf, 3);
 
     /* Fill frame descriptor const values. */
     desc_.depth = 2;
@@ -43,8 +91,23 @@ namespace camera
 
     exposure_time_ = pt.get<float>("pco-edge.exposure_time", exposure_time_);
     triggermode_ = pt.get<WORD>("pco-edge.trigger_mode", triggermode_);
-    if (triggermode_ < 3)
-      triggermode_ = 0;
+    // TODO : Check if removing this solves the triggering problem!
+    //if (triggermode_ < 3)
+    //  triggermode_ = 0;
+    framerate_ = pt.get<DWORD>("pco-edge.framerate", framerate_) * 10e3;
+    framerate_mode_ = pt.get<WORD>("pco-edge.framerate_mode", framerate_mode_);
+    p0_x_ = pt.get<WORD>("pco-edge.p0_x", p0_x_);
+    p0_y_ = pt.get<WORD>("pco-edge.p0_y", p0_y_);
+    p1_x_ = pt.get<WORD>("pco-edge.p1_x", p1_x_);
+    p1_y_ = pt.get<WORD>("pco-edge.p1_y", p1_y_);
+    hz_binning_ = pt.get<WORD>("pco-edge.binning_hz", hz_binning_);
+    vt_binning_ = pt.get<WORD>("pco-edge.binning_vt", vt_binning_);
+    pixel_rate_ = pt.get<DWORD>("pco-edge.pixel_rate", pixel_rate_) * 10e6;
+    conversion_factor_ = static_cast<WORD>(
+        pt.get<float>("pco-edge.conversion_factor", conversion_factor_) * 100);
+    ad_converters_ = pt.get<WORD>("pco-edge.adc", ad_converters_);
+    timeouts_[0] = pt.get<unsigned int>("pco-edge.timeout_command", timeouts_[0]);
+    timeouts_[1] = pt.get<unsigned int>("pco-edge.timeout_img_acq", timeouts_[1]);
   }
 
   void CameraPCOEdge::bind_params()
@@ -52,7 +115,14 @@ namespace camera
     int status = PCO_NOERROR;
 
     status |= PCO_ResetSettingsToDefault(device_);
+
+    status |= PCO_SetTimeouts(device_, timeouts_, 2);
     status |= PCO_SetSensorFormat(device_, 0);
+    status |= PCO_SetADCOperation(device_, ad_converters_);
+    status |= PCO_SetConversionFactor(device_, ad_converters_);
+    status |= PCO_SetBinning(device_, hz_binning_, vt_binning_);
+    status |= PCO_SetROI(device_, p0_x_, p0_y_, p1_x_, p1_y_);
+    status |= PCO_SetPixelRate(device_, pixel_rate_);
     status |= PCO_SetTriggerMode(device_, triggermode_);
     status |= PCO_SetNoiseFilterMode(device_, 0);
 
@@ -69,7 +139,19 @@ namespace camera
       status |= PCO_SetDelayExposureTime(device_, 0, static_cast<DWORD>(exposure_time_), 0, base_time);
     }
 
+    {
+      WORD fps_change_status;
+      DWORD exp_time = static_cast<DWORD>(exposure_time_ * 10e9);
+      int res = PCO_SetFrameRate(device_, &fps_change_status, 0, &framerate_, &exp_time);
+      // DEBUG : Not sure the camera actually implements this function.
+      if (res != PCO_NOERROR)
+        std::cerr << "PCO_SetFrameRate() call failed : error code " << res << std::endl;
+      // ! DEBUG
+      exposure_time_ = static_cast<float>(exp_time) * 10e-9; // Convert from nanoseconds
+    }
+
     if (status != PCO_NOERROR)
       throw CameraException(CameraException::CANT_SET_CONFIG);
   }
+
 }
--- Camera/CameraPCOEdge/camera_pco_edge.hh
+++ Camera/CameraPCOEdge/camera_pco_edge.hh
@@ -29,6 +29,90 @@ namespace camera
      *  * 0x0003: extern exposure control.
      */
     WORD triggermode_;
+
+    /* Number of images recorded per millisecond.
+    ** Please note that in the .ini file, fps are expressed
+    ** in frames per second (less awkward).
+    */ 
+    DWORD framerate_;
+    // Computation mode (adjusting between exposure time and fps)
+    WORD framerate_mode_;
+
+    /* Binning mode. Binning value can be 1 pixel (no binning),
+    ** or 2 or 4 pixels, for each dimension; e.g. binning of 2x4
+    ** means aggregating 2 pixels horizontally and 4 vertically
+    ** to obtain one pixel in the output image.
+    */
+    WORD hz_binning_;
+    WORD vt_binning_;
+
+    /* Region Of Interest (ROI) selection.
+    ** The ROI shall be no greater than the camera's frame dimensions.
+    ** Using a ROI speeds up image readout by limiting the number of pixels
+    ** which need to be output, but reduces accuracy.
+    **
+    ** Points p0 and p1 define the rectangle zone of the ROI, like this :
+    **
+    **   (p0) *---------------------*
+    **        |                     |
+    **        |                     |
+    **        |                     |
+    **        *---------------------* (p1)
+    **
+    ** Please note that ROI settings depend on binning and
+    ** sensor format.
+    ** e.g. binning of 2x2 on a 1600x1200 image makes maximum
+    ** horizontal value to 800 and vertical to 600.
+    */
+    WORD p0_x_;
+    WORD p0_y_;
+
+    WORD p1_x_;
+    WORD p1_y_;
+
+    /* Also called pixel clock frequency.
+    ** Frequency in Hertz determining image readout frequency.
+    ** For available values, please  see the camera's manual.
+    ** In the .ini file, this is expressed in MHz for commodity.
+    */
+    DWORD pixel_rate_;
+
+    /* Conversion factor : the amount of electrons per "count"
+    ** when interpreting electron quantity as light intensity.
+    ** In our context (interferometry), a large dynamic range is
+    ** already provided (the camera is very exposed to light),
+    ** so this value is very low.
+    */
+    WORD conversion_factor_;
+
+    /* Number of Analog to Digital Converters (ADC) to be used
+    ** when performing readout of sensors.
+    ** Several converters allow for faster (parallelized) readout
+    ** at the expense of worse linearity.
+    */
+    WORD ad_converters_ = 1;
+
+    /* Timeout configurations
+    ** The SDK handles this with an array of size 3.
+    ** (the third parameter only concerns Firewire interfaces,
+    ** so we don't care about it).
+    ** [0] Command timeout
+    ** [1] Image request timeout
+    **
+    ** After x milliseconds without a response from the camera,
+    ** abort command execution / image acquisition request.
+    */
+    unsigned int timeouts_[3];
+
+    /* Hardware input/output configuration
+    ** The Edge 4.2 LT model comprises 4 I/O hardware links.
+    ** Each set of options for one I/O interface corresponds to
+    ** a PCO_Signal (see sc2_SDKStructures.h) struct.
+    */
+    PCO_Signal io_1_conf;
+    PCO_Signal io_2_conf;
+    PCO_Signal io_3_conf;
+    PCO_Signal io_4_conf;
   };
 }
 
