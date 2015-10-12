f79df19077cd6eb4150559322cd5b0d495cf1a31
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-10-12 14:18:48 +0200

    Adding empty class for Adimec camera.

---
+++ Camera/CameraAdimec/camera_adimec.hh
@@ -0,0 +1,15 @@
+#ifndef CAMERA_ADIMEC_HH
+# define CAMERA_ADIMEC_HH
+
+#include <camera.hh>
+
+namespace camera
+{
+  class CameraAdimec : public Camera
+  {
+  public:
+  private:
+  };
+}
+
+#endif /* !CAMERA_ADIMEC_HH */
