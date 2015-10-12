d8d9dc67098f166e24bfcfe8db1238767f9b288f
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-10-12 17:20:12 +0200

    Fixed board finding.

--- Camera/CameraAdimec/camera_adimec.hh
+++ Camera/CameraAdimec/camera_adimec.hh
@@ -1,14 +1,39 @@
 #ifndef CAMERA_ADIMEC_HH
 # define CAMERA_ADIMEC_HH
 
+#include <BFType.h>
+
 #include <camera.hh>
 
 namespace camera
 {
+  /* Adimec camera, used through a BitFlow CoaXPress frame grabber.
+  ** The API used is the Ci API, because it allows for broad configuration
+  ** of the used camera without dwelving into low-level details.
+  ** See BitFlow's SDK for further details about the different APIs available
+  ** for BitFlow products.
+  */
   class CameraAdimec : public Camera
   {
   public:
+    CameraAdimec();
+    virtual ~CameraAdimec();
+
+    // ICamera's main methods.
+    virtual void init_camera() override;
+    virtual void start_acquisition() override;
+    virtual void stop_acquisition() override;
+    virtual void shutdown_camera() override;
+    virtual void* get_frame() override;
+
   private:
+    // Camera's camera-specific methods, used while initializion takes place.
+    virtual void load_ini_params() override;
+    virtual void load_default_params() override;
+    virtual void bind_params() override;
+
+    // Handle to the opened BitFlow board.
+    Bd board_;
   };
 }
 
