6d26bf7651c01a78e7467a5bc7666771f508f059
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-10-13 11:57:09 +0200

    Added start_acquisition; also filled frame descriptor.

--- Camera/CameraAdimec/camera_adimec.cc
+++ Camera/CameraAdimec/camera_adimec.cc
@@ -1,6 +1,7 @@
 #include <CiApi.h>
 // DEBUG
 #include <iostream>
+#include <cmath>
 
 #include "camera_adimec.hh"
 #include "camera_exception.hh"
@@ -17,9 +18,15 @@ namespace camera
 
   CameraAdimec::CameraAdimec()
     : Camera("adimec.ini")
-    , board_{ nullptr }
+    , board_ { nullptr }
+  , buffer_ { nullptr }
   {
     name_ = "Adimec";
+    desc_.width = 1440;
+    desc_.height = 1440;
+    desc_.depth = 12;
+    desc_.endianness = LITTLE_ENDIAN;
+    // TODO : Give pixel size.
 
     load_default_params();
     if (ini_file_is_open())
@@ -54,10 +61,47 @@ namespace camera
 
   void CameraAdimec::start_acquisition()
   {
+    /* Remember that bit depth is in bits, not bytes; thus we have to divide
+    ** by 8 appropriately.
+    */
+    const unsigned buffer_pitch = static_cast<unsigned>(ceil(desc_.width * (desc_.depth / 8.0f)));
+    buffer_ = new char[buffer_pitch * buffer_pitch];
+    if (!buffer_)
+      throw CameraException(CameraException::MEMORY_PROBLEM);
+
+    BFRC status = CiAqSetup(board_,
+      buffer_,
+      buffer_pitch * buffer_pitch,
+      buffer_pitch,
+      CiDMADataMem, // We don't care about this parameter, it is for another board.
+      CiLutBank0,
+      CiLut12Bit,
+      CiQTabBank0, // We use a single buffer, in a single bank.
+      true,
+      CiQTabModeOneBank, // We don't need to use several cameras at the same time.
+      AqEngJ // We dont' care about this parameter, it is for another board.
+      );
+    if (status != CI_OK)
+    {
+      std::cerr << "[CAMERA] Could not setup board for acquisition." << std::endl;
+      delete buffer_;
+      CiBrdClose(board_);
+      throw CameraException(CameraException::CANT_START_ACQUISITION);
+    }
   }
 
   void CameraAdimec::stop_acquisition()
   {
+    /* Free resources taken by CiAqSetup, in a single function call.
+    ** However, the allocated buffer has to be freed manually.
+    */
+    delete buffer_;
+    if (CiAqCleanUp(board_, AqEngJ) != CI_OK)
+    {
+      std::cerr << "[CAMERA] Could not stop acquisition cleanly." << std::endl;
+      CiBrdClose(board_);
+      throw CameraException(CameraException::CANT_STOP_ACQUISITION);
+    }
   }
 
   void CameraAdimec::shutdown_camera()
@@ -67,7 +111,7 @@ namespace camera
 
   void* CameraAdimec::get_frame()
   {
-    return nullptr;
+    return buffer_;
   }
 
   /* Private methods
