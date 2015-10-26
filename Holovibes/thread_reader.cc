0a7f8104ddea49530a388fe2b05b1c32c065f324
Eric <9ryu108@gmail.com>
2015-10-26 16:23:14 +0100

    cleanup; use seekg insdead of close/open; read multi frame instead of ligne by ligne

--- Holovibes/thread_reader.cc
+++ Holovibes/thread_reader.cc
@@ -2,80 +2,88 @@
 # include <fstream>
 # include <Windows.h>
 
+#include <chrono>
+
 namespace holovibes
 {
-	ThreadReader::ThreadReader(std::string file_src
-		, ThreadReader::FrameDescriptor frame_desc
-		, bool loop
-		, unsigned int fps
-		, unsigned int spanStart
-		, unsigned int spanEnd
-		, Queue& input)
-		: IThreadInput()
-		, file_src_(file_src)
-		, frame_desc_(frame_desc.desc)
-		, desc_(frame_desc)
-		, loop_(loop)
-		, fps_(fps)
-		, frameId_(0)
-		, spanStart_(spanStart)
-		, spanEnd_(spanEnd)
-		, queue_(input)
-		, thread_(&ThreadReader::thread_proc, this)
-	{
-	}
+  ThreadReader::ThreadReader(std::string file_src
+    , ThreadReader::FrameDescriptor frame_desc
+    , bool loop
+    , unsigned int fps
+    , unsigned int spanStart
+    , unsigned int spanEnd
+    , Queue& input)
+    : IThreadInput()
+    , file_src_(file_src)
+    , frame_desc_(frame_desc.desc)
+    , desc_(frame_desc)
+    , loop_(loop)
+    , fps_(fps)
+    , frameId_(0)
+    , spanStart_(spanStart)
+    , spanEnd_(spanEnd)
+    , queue_(input)
+    , thread_(&ThreadReader::thread_proc, this)
+  {
+  }
 
-	void	ThreadReader::thread_proc()
-	{
-		std::ifstream	ifs(file_src_, std::istream::in | std::ifstream::binary);
-		unsigned int frame_size = frame_desc_.width * frame_desc_.height * frame_desc_.depth;
-		char* buffer = new char[frame_size];
+  void	ThreadReader::thread_proc()
+  {
+    std::ifstream	ifs(file_src_, std::istream::in | std::ifstream::binary);
+    std::streampos pos;
+    unsigned int frame_size = frame_desc_.width * frame_desc_.height * frame_desc_.depth;
+    char* buffer = new char[frame_size * NBR];
+    unsigned int nbr_stored = 0;
+    unsigned int act_frame = 0;
 
-		for (unsigned int i = 0; i < frame_size; ++i)
-			buffer[i] = 0;
+    try
+    {
+      if (!ifs.is_open())
+        throw std::runtime_error("[READER] unable to read/open file: " + file_src_);
+      while (++frameId_ < spanStart_)
+        ifs.read(buffer, frame_size);
+      pos = ifs.tellg();
 
-		try
-		{
-			while (!stop_requested_)
-			{
-				if (!ifs.is_open())
-					throw std::runtime_error("[READER] unable to read/open file: " + file_src_);
-				if (ifs.good() && frameId_ < spanEnd_)
-				{
-					do {
-						for (unsigned int y = 0; y < desc_.img_height; ++y)
-							ifs.read(buffer + y * frame_desc_.width * frame_desc_.depth, desc_.img_width * frame_desc_.depth);
-					} while (++frameId_ < spanStart_);
+      while (!stop_requested_)
+      {
+        if (ifs.good() && frameId_ <= spanEnd_)
+        {
+          if (act_frame >= nbr_stored)
+          {
+            ifs.read(buffer, frame_size * NBR);
+            nbr_stored = ifs.gcount() / frame_size;
+            act_frame = 0;
+          }
+          queue_.enqueue(buffer + act_frame * frame_size, cudaMemcpyHostToDevice);
+          ++frameId_;
+          ++act_frame;
+          Sleep(1000 / fps_);
+        }
+        else if (loop_)
+        {
+          ifs.clear();
+          ifs.seekg(pos);
+          frameId_ = spanStart_;
+          act_frame = 0;
+        }
+        else
+          stop_requested_ = true;
+      }
+    }
+    catch (std::runtime_error& e)
+    {
+      std::cout << e.what() << std::endl;
+    }
+    if (ifs.is_open())
+      ifs.close();
+    stop_requested_ = true;
+    delete[] buffer;
+  }
 
-					queue_.enqueue(buffer, cudaMemcpyHostToDevice);
-					Sleep(1000 / fps_);
-				}
-				else
-				{
-					if (loop_)
-					{
-						if (ifs.is_open())
-							ifs.close();
-						ifs.open(file_src_, std::istream::in | std::ifstream::binary);
-						frameId_ = 0;
-					}
-				}
-			}
-		}
-		catch (std::runtime_error& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-		if (ifs.is_open())
-			ifs.close();
-		stop_requested_ = true;
-		delete[] buffer;
-	}
-
-	ThreadReader::~ThreadReader()
-	{
-		stop_requested_ = true;
-		if (thread_.joinable())
-			thread_.join();
-	}
+  ThreadReader::~ThreadReader()
+  {
+    stop_requested_ = true;
+    if (thread_.joinable())
+      thread_.join();
+  }
 }
--- Holovibes/thread_reader.hh
+++ Holovibes/thread_reader.hh
@@ -1,4 +1,3 @@
-
 #ifndef THREAD_READER_HH
 #define THREAD_READER_HH
 
@@ -9,88 +8,92 @@
 # include "queue.hh"
 # include "ithread_input.hh"
 
+/*! Max number of frames read each time
+ ** Use in order to limit disc usage
+ */
+#define NBR 5
+
 namespace holovibes
 {
-	class ThreadReader : public IThreadInput
-	{
-	public:
+  class ThreadReader : public IThreadInput
+  {
+  public:
 
-		struct FrameDescriptor
-		{
-		public:
-			camera::FrameDescriptor	desc;
-			/*! Width of the image. != frame width */
-			unsigned short         img_width;
-			/*! Height of the image. != frame height */
-			unsigned short         img_height;
+    struct FrameDescriptor
+    {
+    public:
+      camera::FrameDescriptor	desc;
+      /*! Width of the image. != frame width */
+      unsigned short         img_width;
+      /*! Height of the image. != frame height */
+      unsigned short         img_height;
 
+      /*
+        http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c
+        http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        */
+      inline bool isPowerOfTwo(unsigned int x) const
+      {
+        return ((x != 0) && ((x & (~x + 1)) == x));
+      }
 
-			/*
-				http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c
-				http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-			*/
-			inline bool isPowerOfTwo(unsigned int x) const
-			{
-				return ((x != 0) && ((x & (~x + 1)) == x));
-			}
+      inline unsigned int nextHightestPowerOf2(unsigned int x) const
+      {
+        x--;
+        x |= x >> 1;
+        x |= x >> 2;
+        x |= x >> 4;
+        x |= x >> 8;
+        x |= x >> 16;
+        x++;
+        return (x);
+      }
 
-			inline unsigned int nextHightestPowerOf2(unsigned int x) const
-			{
-				x--;
-				x |= x >> 1;
-				x |= x >> 2;
-				x |= x >> 4;
-				x |= x >> 8;
-				x |= x >> 16;
-				x++;
-				return (x);
-			}
+      void		compute_sqared_image(void)
+      {
+        unsigned short	biggestBorder = (desc.width > desc.height ? desc.width : desc.height);
 
-			void		compute_sqared_image(void)
-			{
-				unsigned short	biggestBorder = (desc.width > desc.height ? desc.width : desc.height);
+        img_width = desc.width;
+        img_height = desc.height;
 
-				img_width = desc.width;
-				img_height = desc.height;
+        if (desc.width != desc.height)
+          desc.width = desc.height = biggestBorder;
 
-				if (desc.width != desc.height)
-					desc.width = desc.height = biggestBorder;
+        if (!isPowerOfTwo(biggestBorder))
+          desc.width = desc.height = static_cast<unsigned short>(nextHightestPowerOf2(biggestBorder));
+      }
 
-				if (!isPowerOfTwo(biggestBorder))
-					desc.width = desc.height = static_cast<unsigned short>(nextHightestPowerOf2(biggestBorder));
-			}
+      FrameDescriptor(camera::FrameDescriptor d)
+        : desc(d)
+        , img_width(d.width)
+        , img_height(d.height)
+      {
+        this->compute_sqared_image();
+      }
+    };
 
-			FrameDescriptor(camera::FrameDescriptor d)
-				: desc(d)
-				, img_width(d.width)
-				, img_height(d.height)
-			{
-				this->compute_sqared_image();
-			}
-		};
+    ThreadReader(std::string file_src
+      , holovibes::ThreadReader::FrameDescriptor frame_desc
+      , bool loop
+      , unsigned int fps
+      , unsigned int spanStart
+      , unsigned int spanEnd
+      , Queue& input);
+    virtual ~ThreadReader();
 
-		ThreadReader(std::string file_src
-			, holovibes::ThreadReader::FrameDescriptor frame_desc
-			, bool loop
-			, unsigned int fps
-			, unsigned int spanStart
-			, unsigned int spanEnd
-			, Queue& input);
-		virtual ~ThreadReader();
+    void  thread_proc(void);
 
-		void  thread_proc(void);
+    std::string file_src_;
+    bool loop_;
+    unsigned int fps_;
+    camera::FrameDescriptor frame_desc_;
+    holovibes::ThreadReader::FrameDescriptor	desc_;
+    unsigned int frameId_;
+    unsigned int spanStart_;
+    unsigned int spanEnd_;
+    Queue& queue_;
 
-		std::string file_src_;
-		bool loop_;
-		unsigned int fps_;
-		camera::FrameDescriptor frame_desc_;
-		holovibes::ThreadReader::FrameDescriptor	desc_;
-		unsigned int frameId_;
-		unsigned int spanStart_;
-		unsigned int spanEnd_;
-		Queue& queue_;
-
-		std::thread thread_;
-	};
+    std::thread thread_;
+  };
 }
 #endif
