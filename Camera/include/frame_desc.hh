bd40ab6ac98b87a2d8d48ba02815354e93684b10
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-11-04 17:26:50 +0100

    Updated FrameDescriptor.

--- Camera/include/frame_desc.hh
+++ Camera/include/frame_desc.hh
@@ -8,30 +8,31 @@ namespace camera
     LITTLE_ENDIAN
   } e_endianness;
 
-  /*! \brief This structure contains everything related to the image format. */
+  /*! This structure contains everything related to the format of the images
+   * captured by the current camera.
+   * Changing the camera used changes the frame descriptor, which will be used
+   * in the rendering window and the holograms computations. */
   struct FrameDescriptor
   {
   public:
-    /*! Width of the frame. */
-    unsigned short         width;
-    /*! Height of the frame. */
-    unsigned short         height;
-    /*! Byte depth during acquisition. */
-    float         depth;
-    /*! Size of pixels in micrometers. */
-    float                pixel_size;
-    /*! Endianness of bytes. */
-    e_endianness         endianness;
+    unsigned short width; //!< Width of the frame in pixels.
 
-    /* Helper functions. */
+    unsigned short height; //!< Height of the frame in pixels.
 
-    /*! \brief Return the frame size in bytes. */
+    float depth; //!< Byte depth during acquisition.
+
+    float pixel_size; //!< Size of pixels in micrometers.
+
+    //!< To each camera software its endianness. Useful for 16-bit cameras.
+    e_endianness endianness;
+
+    //!< Obtain the total frame size in bytes.
     unsigned int frame_size() const
     {
       return width * height * depth;
     }
 
-    /*! \brief Return the frame resolution (number of pixels). */
+    //!< \brief Return the frame resolution (number of pixels).
     unsigned int frame_res() const
     {
       return width * height;
