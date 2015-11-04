e5f9634d5526de2460c8df4a2edecd581100ecb0
Arnaud GAILLARD <arnaud.gaillard@espci.fr>
2015-11-04 15:01:00 +0100

    doc : added which lens goes with which fft*

--- Holovibes/transforms.cuh
+++ Holovibes/transforms.cuh
@@ -5,7 +5,7 @@
 # include <cufft.h>
 # include <frame_desc.hh>
 
-/*! \brief Compute a lens to apply to an image
+/*! \brief Compute a lens to apply to an image used by the fft1
 *
 * \param output The lens computed by the function.
 * \param fd File descriptor of the images on which the lens will be applied.
@@ -18,7 +18,7 @@ __global__ void kernel_quadratic_lens(
   float lambda,
   float dist);
 
-/*! \brief Compute a lens to apply to an image
+/*! \brief Compute a lens to apply to an image used by the fft2
 *
 * \param output The lens computed by the function.
 * \param fd File descriptor of the images on wich the lens will be applied.
