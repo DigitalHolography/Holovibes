af9916994dfc5edc7dfc173fbffb529238adc7a3
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-11-05 10:11:49 +0100

    Updated CameraUtils, added it to files processed by Doxygen.

--- CameraUtils/utils.hh
+++ CameraUtils/utils.hh
@@ -9,34 +9,55 @@
 # include <fstream>
 # include <string>
 
-/* A set of various functions usable by all cameras if they wish to.
-** This module factorizes common needs of cameras implementations.
-*/
 namespace camutils
 {
   std::fstream logfile;
   static std::string filename;
 
+  /*! \defgroup CamUtils Camera Utilities
+  * Camera utilities functions : various functions usable by cameras DLLs
+  * to respond to their needs.
+  *
+  * Services provided are :
+  * * Logging activity into a file
+  * * Asking for pinned memory allocation on the host to speed up data transfer.
+  * \{
+  */
   extern "C"
   {
-    /* Logging functions
-    */
+    /*! \brief Create a logfile for a given camera name.
+     *
+     * The file will be created in a log/ directory in the current
+     * working directory of the executable. */
     UTILS_API void create_logfile(std::string name);
 
+    /*! \brief Log a single message. */
     UTILS_API void log_msg(std::string msg);
 
+    /*! \brief The user of the logging service has to manually close the stream.
+     *
+     * If the logfile is empty because no event was logged, it is deleted
+     * to avoid cluttering. */
     UTILS_API void close_logfile();
 
-    /* "Pinned" memory allocation/deallocation on host RAM.
-    ** Pinned (non-pageable) is required for data moving from host to GPU
-    ** or the other way around, as GPUs do not handle other kinds of RAM memory.
-    ** Setting such a memory to be non-pageable right from the allocation step
-    ** allow faster memory accesses, and so faster memory copies.
-    **
-    ** \param size The number of bytes to allocate.
-    */
+    /*! \brief "Pinned" memory allocation/deallocation on host RAM.
+     *
+     * Pinned (non-pageable) memory is required for data moving from host RAM
+     * to GPU RAM or the other way around, as GPUs do not handle other kinds
+     * of RAM memory. In practice, using pageable memory on RAM forces
+     * preparing a pinned intermediate memory space at each memory transaction.
+     *
+     * Setting allocated memory to be non-pageable right from the start allows
+     * faster memory copies.
+     *
+     * \param buf The address of the pointer to the memory space needed.
+     * If allocation fails, buf will point to a nullptr.
+     * \param size The number of bytes to allocate.
+     */
     UTILS_API void allocate_memory(void** buf, const std::size_t size);
 
+    /*! \brief Memory allocated with allocate_memory() MUST be freed with this function. */
     UTILS_API void free_memory(void* buf);
   }
+  /*! \} */ // End of CamUtils group
 }
