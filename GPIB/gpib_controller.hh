67e1dca73ab8796457ce77e399f797ec0c8b0f67
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2015-11-16 10:18:16 +0100

    Added more GPIB exceptions, and made Holovibes use them.

--- GPIB/gpib_controller.hh
+++ GPIB/gpib_controller.hh
@@ -8,38 +8,50 @@
 
 # define BUF_SIZE 200
 
-/*! Contains all elements needed to establish
- * a connection to a device through the VISA interface. */
-class VisaInterface
+namespace gpib
 {
-public:
-  VisaInterface(const std::string& path);
+  /*! Contains all elements needed to establish
+   * a connection to a device through the VISA interface. */
+  class VisaInterface
+  {
+  public:
+    /*! Build an interface to the GPIB driver using the VISA standard.
+     * \param path Path to the batch file that is provided to control
+     * images recording using GPIB-driven components. */
+    VisaInterface(const std::string& path);
 
-  ~VisaInterface();
+    ~VisaInterface();
 
-  /*! Setting up the connection with an instrument at a given address. */
-  void initialize_instr(const unsigned address);
+    /*! Setting up the connection with an instrument at a given address. */
+    void initialize_instr(const unsigned address);
 
-  /*! Closing the connection with a given instrument, knowing its address. */
-  void close_instr(const unsigned address);
+    /*! Closing the connection with a given instrument, knowing its address. */
+    void close_instr(const unsigned address);
 
-  /*! Launch the commands extracted previously from the input file. */
-  int execute_next_block();
+    /*! Launch the commands extracted previously from the input file. */
+    int execute_next_block();
 
-private:
-  /*! Setting up the VISA driver to enable future connections.
-   * Automatically called by the constructor. */
-  void initialize_line();
+  private:
+    /*! Setting up the VISA driver to enable future connections.
+     * Automatically called by the constructor. */
+    void initialize_line();
 
-  /*! Closing the connection to the VISA driver.
-   * Automatically called by the destructor. */
-  void close_line();
+    /*! Closing the connection to the VISA driver.
+     * Automatically called by the destructor. */
+    void close_line();
 
-private:
-  struct VisaPimpl;
-  VisaPimpl* pimpl_;
+  private:
+    /*! To decouple dependencies between the GPIB controller and Holovibes,
+     * a kind of Pimpl idiom is used. In this case, we do not use an intermediate
+     * Pimpl class, but just regroup all VISA-related types in a structure,
+     * and include visa.h in the implementation file (gpib_controller.cc).
+     * Hence Holovibes is not dependent of visa.h by including gpib_controller.hh.
+     */
+    struct VisaPimpl;
+    VisaPimpl* pimpl_;
 
-  /*! Lines obtained from the batch input file are stored
-   * here as separate strings. */
-  std::deque<std::string> batch_cmds_;
-};
+    /*! Lines obtained from the batch input file are stored
+     * here as separate strings. */
+    std::deque<std::string> batch_cmds_;
+  };
+}
