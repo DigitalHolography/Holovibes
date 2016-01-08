a5fac7988ac191123d2f23af8c0af8c598a0dddf
Geoffrey Le Gourriérec <geoffrey.legourrierec@epita.fr>
2016-01-08 11:12:43 +0100

    Moved CUDA kernels related to unwrapping in separate files.

---
+++ Holovibes/tools_unwrap.cu
@@ -0,0 +1,100 @@
+#include <device_launch_parameters.h>
+#include <cmath>
+# ifndef _USE_MATH_DEFINES
+/* Enables math constants. */
+#  define _USE_MATH_DEFINES
+# endif /* !_USE_MATH_DEFINES */
+
+#include "tools_unwrap.cuh"
+
+__global__ void kernel_extract_angle(
+  const cufftComplex* input,
+  float* output,
+  const size_t size)
+{
+  const unsigned index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index >= size)
+    return;
+
+  // We use std::atan2 in order to obtain results in [-pi; pi].
+  output[index] = std::atan2(input[index].y, input[index].x);
+}
+
+__global__ void kernel_unwrap(
+  float* pred,
+  float* cur,
+  float* adjustments,
+  const size_t size)
+{
+  const unsigned index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index >= size)
+    return;
+  const float pi = M_PI;
+
+  float local_diff = cur[index] - pred[index];
+  // Unwrapping //
+  float local_adjust;
+  if (local_diff > pi)
+    local_adjust = -2.f * pi;
+  else if (local_diff < -pi)
+    local_adjust = 2.f * pi;
+  else
+    local_adjust = 0.f;
+
+  // Cumulating the phase correction with precedent ones //
+  adjustments[index] = local_adjust;
+}
+
+__global__ void kernel_compute_angle_mult(
+  const cufftComplex* pred,
+  const cufftComplex* cur,
+  float* output,
+  const size_t size)
+{
+  const unsigned index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index >= size)
+    return;
+
+  cufftComplex diff;
+  diff = cur[index];
+  diff.x *= pred[index].x;
+  diff.y *= -1.f * pred[index].y;
+
+  output[index] = std::atan2(diff.y, diff.x);
+}
+
+__global__ void kernel_compute_angle_diff(
+  const cufftComplex* pred,
+  const cufftComplex* cur,
+  float* output,
+  const size_t size)
+{
+  const unsigned index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index >= size)
+    return;
+
+  cufftComplex diff;
+  diff = cur[index];
+  diff.x -= pred[index].x;
+  diff.y -= pred[index].y;
+
+  output[index] = std::atan2(diff.y, diff.x);
+}
+
+__global__ void kernel_correct_angles(
+  float* data,
+  const float* corrections,
+  const size_t image_size,
+  const size_t history_size)
+{
+  const unsigned index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index >= image_size)
+    return;
+
+  for (auto correction_idx = index;
+    correction_idx < history_size * image_size;
+    correction_idx += image_size)
+  {
+    data[index] += corrections[correction_idx];
+  }
+}
