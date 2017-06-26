8ce1c13e072dd0982fcfff7567accf177088ba61
abartz <celuiquicode42@gmail.com>
2017-06-26 12:10:15 +0200

    [Up] Delete useless test file

--- Holovibes/sources/gui/windows/MainWindow.cc
+++ Holovibes/sources/gui/windows/MainWindow.cc
@@ -2822,5 +2822,43 @@ namespace holovibes
 			import_endian_box->setCurrentIndex(endian);
 		}
 		#pragma endregion
+
+		#pragma region Themes
+		void MainWindow::set_night()
+		{
+			qApp->setStyle(QStyleFactory::create("Fusion"));
+
+			QPalette darkPalette;
+			darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+			darkPalette.setColor(QPalette::WindowText, Qt::white);
+			darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+			darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+			darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+			darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+			darkPalette.setColor(QPalette::Text, Qt::white);
+			darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+			darkPalette.setColor(QPalette::ButtonText, Qt::white);
+			darkPalette.setColor(QPalette::BrightText, Qt::red);
+			darkPalette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+			darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+			darkPalette.setColor(QPalette::Disabled, QPalette::WindowText, Qt::darkGray);
+			darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+			darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+			darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+			qApp->setPalette(darkPalette);
+
+			qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+			theme_index_ = 1;
+		}
+
+		void MainWindow::set_classic()
+		{
+			qApp->setPalette(this->style()->standardPalette());
+			qApp->setStyle(QStyleFactory::create("WindowsVista"));
+			qApp->setStyleSheet("");
+			theme_index_ = 0;
+		}
+		#pragma endregion
 	}
 }
