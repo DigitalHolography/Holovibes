5443d3a232ee6cdafb46d0756bc07e3a4dbe69a8
Julien GAUTIER <gautier.julien@gmail.com>
2017-12-11 15:23:21 +0100

    add: Drag&Drop ability to all LineEdit accepting file path as input

--- Holovibes/includes/gui/other/gui_drag_drop_lineedit.hh
+++ Holovibes/includes/gui/other/gui_drag_drop_lineedit.hh
@@ -6,16 +6,11 @@ namespace holovibes::gui {
 		Q_OBJECT
 	public:
 		Drag_drop_lineedit(QWidget* parent = nullptr);
-			
-		//Q_SIGNALS:
+		
 		public slots:
 		void dropEvent(QDropEvent* event) override;
 		void dragEnterEvent(QDragEnterEvent* e) override {
 			e->acceptProposedAction();
-			/*if (e->mimeData()->hasFormat("application/x-qabstractitemmodeldatalist")) {
-				e->acceptProposedAction();
-			}*/
 		}
-		//void dragMoveEvent(QDragMoveEvent event) override;
 	};
 }
--- Holovibes/sources/gui/other/gui_drag_drop_lineedit.cc
+++ Holovibes/sources/gui/other/gui_drag_drop_lineedit.cc
@@ -6,24 +6,15 @@ namespace holovibes::gui {
 	Drag_drop_lineedit::Drag_drop_lineedit(QWidget * parent)
 		: QLineEdit(parent)
 	{
-		setAcceptDrops(true);
-		setDragEnabled(false);
+		setPlaceholderText("Drop file here");
 	}
 
 	void Drag_drop_lineedit::dropEvent(QDropEvent * event)
 	{
-		//const QMimeData* mimeData = event->mimeData();
 		auto url = event->mimeData()->urls()[0];
 		auto path = url.path();
 		if (path.at(0) == '/')
 			path.remove(0, 1);
 		setText(path);
 	}
-
-	/*void Drag_drop_lineedit::dragMoveEvent(QDragMoveEvent event)
-	{
-		const QMimeData* mimeData = event->mimeData();
-	}*/
 }
-
-//#include "moc_gui_drag_drop_lineedit.cc"
