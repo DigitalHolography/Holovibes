32c2849195ffdf818f845af76c26652d8642c6e9
Loïc Bellonnet-Mottet <loic.bellonnet-mottet@epita.fr>
2019-09-18 11:51:12 +0200

    fix: Only copy holovibes.ini if it exists

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
 CONFIG_TYPE=Debug
-if [ $# -ge 1 ] && [ $1 == "Release" ]; then
-	CONFIG_TYPE="$1"
-	shift 1
+if [ $# -ge 1 ]; then
+	case $1 in
+		"Release")
+			CONFIG_TYPE=Release
+			shift 1
+			;;
+		"R")
+			CONFIG_TYPE=Release
+			shift 1
+			;;
+		"Debug")
+			CONFIG_TYPE=Debug
+			shift 1
+			;;
+		"D")
+			CONFIG_TYPE=Debug
+			shift 1
+			;;
+	esac
 fi
 
-echo "cmake -B build -S . -A x64"
-cmake -B build -S . -A x64
-
-echo "cmake --build build --config ${CONFIG_TYPE} $@ -- /verbosity:normal"
-cmake --build build --config "${CONFIG_TYPE}" $@ -- /verbosity:normal
+echo "cmake -B build -S . -A x64 && cmake --build build --config ${CONFIG_TYPE} $@ -- /verbosity:normal"
+cmake -B build -S . -A x64 && cmake --build build --config "${CONFIG_TYPE}" $@ -- /verbosity:normal
--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
 CONFIG_TYPE=Debug
-if [ $# -ge 1 ] && [ $1 == "Release" ]; then
-	CONFIG_TYPE="$1"
+if [ $# -ge 1 ]; then
+	case $1 in
+		"Release")
+			CONFIG_TYPE=Release
+			shift 1
+			;;
+		"R")
+			CONFIG_TYPE=Release
+			shift 1
+			;;
+		"Debug")
+			CONFIG_TYPE=Debug
+			shift 1
+			;;
+		"D")
+			CONFIG_TYPE=Debug
+			shift 1
+			;;
+	esac
 fi
 
 echo "build/$CONFIG_TYPE && ./Holovibes.exe"
