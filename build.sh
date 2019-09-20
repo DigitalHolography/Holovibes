5d39284af5e7c4baf8f8ea40fac5e18fb397382c
Loïc Bellonnet-Mottet <loic.bellonnet-mottet@epita.fr>
2019-09-20 16:52:20 +0200

    up: Add option for ninja in build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
 
 CONFIG_TYPE=Debug
-if [ $# -ge 1 ]; then
-	case $1 in
-		"Release")
-			CONFIG_TYPE=Release
-			shift 1
-			;;
-		"R")
-			CONFIG_TYPE=Release
-			shift 1
-			;;
-		"Debug")
-			CONFIG_TYPE=Debug
-			shift 1
-			;;
-		"D")
-			CONFIG_TYPE=Debug
-			shift 1
-			;;
-	esac
-fi
+GENERATOR="Visual Studio 15"
 
-echo "cmake -B build -S . -A x64 && cmake --build build --config ${CONFIG_TYPE} $@ -- /verbosity:normal"
-cmake -B build -S . -A x64 && cmake --build build --config "${CONFIG_TYPE}" $@ -- /verbosity:normal
+function check_arg()
+{
+	case $1 in
+		"Release"|"R")
+			CONFIG_TYPE=Release
+			return 1;;
+		"Debug"|"D")
+			CONFIG_TYPE=Debug
+			return 1;;
+		"Ninja"|"N")
+			GENERATOR=Ninja
+			return 1;;
+		*)
+			return 0;;
+	esac
+}
+
+while [ $# -ge 1 ]; do
+	check_arg $1
+	if [ $? -eq 1 ]; then
+		shift 1
+	else
+		break
+	fi
+done
+
+echo "[BUILD.SH] CONFIG_TYPE: ${CONFIG_TYPE}"
+echo "[BUILD.SH] GENERATOR: ${GENERATOR}"
+
+if [ "${GENERATOR}" == "Ninja" ]; then
+	echo "[BUILD.SH] cmd.exe /c" 'call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" && cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE='${CONFIG_TYPE}' -DCMAKE_VERBOSE_MAKEFILE=ON && cmake --build build'
+	cmd.exe /c 'call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" && cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE='${CONFIG_TYPE}' -DCMAKE_VERBOSE_MAKEFILE=ON && cmake --build build'
+else
+	echo "[BUILD.SH] cmake -B build -S . -A x64"
+	cmake -B build -S . -A x64
+	echo "[BUILD.SH] cmake --build build --config ${CONFIG_TYPE} $@ -- /verbosity:normal"
+	cmake --build build --config "${CONFIG_TYPE}" $@ -- /verbosity:normal
+fi
--- run.sh
+++ run.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
 
 CONFIG_TYPE=Debug
-if [ $# -ge 1 ]; then
-	case $1 in
-		"Release")
-			CONFIG_TYPE=Release
-			shift 1
-			;;
-		"R")
-			CONFIG_TYPE=Release
-			shift 1
-			;;
-		"Debug")
-			CONFIG_TYPE=Debug
-			shift 1
-			;;
-		"D")
-			CONFIG_TYPE=Debug
-			shift 1
-			;;
-	esac
-fi
 
-echo "cd build/$CONFIG_TYPE && ./Holovibes.exe"
-cd build/$CONFIG_TYPE && ./Holovibes.exe
+function check_arg()
+{
+	case $1 in
+		"Release"|"R")
+			CONFIG_TYPE=Release
+			return 1;;
+		"Debug"|"D")
+			CONFIG_TYPE=Debug
+			return 1;;
+		*)
+			return 0;;
+	esac
+}
+
+while [ $# -ge 1 ]; do
+	check_arg $1
+	if [ $? -eq 1 ]; then
+		shift 1
+	else
+		break
+	fi
+done
+
+echo "[RUN.SH] CONFIG_TYPE: ${CONFIG_TYPE}"
+
+echo "[RUN.SH] cd build/$CONFIG_TYPE"
+cd build/$CONFIG_TYPE
+
+echo "[RUN.SH] ./Holovibes.exe"
+./Holovibes.exe
