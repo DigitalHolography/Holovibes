2b155f47d86c2d3936ac721f9276279287d4e8fb
Loïc Bellonnet-Mottet <loic.bellonnet-mottet@epita.fr>
2019-10-23 12:15:59 +0200

    up: add support for .holo files in python

--- python/holo.py
+++ python/holo.py
@@ -1,20 +1,60 @@
 from os.path import basename
+from os.path import getsize
 from typing import BinaryIO
-
-
-def load_image(f: BinaryIO, w: int, h: int, nb: int) -> bytes:
-    data = []
-    for _ in range(h):
-        data.append(f.read(nb * w))
-    return data
-
+from struct import unpack
 
 bits_to_bytes = {'8bit': 1, '16bit': 2}
+holo_header_size = 18
+
+class FileData:
+    def __init__(self, fpath: str, parse_output: (int, int, int, int)):
+        self.width = parse_output[0]
+        self.height = parse_output[1]
+        self.bytes_per_pixel = parse_output[2]
+        self.nb_images = parse_output[3]
+        self.fpath = fpath
+        self.size = getsize(fpath)
+        self.is_holo = is_holo(fpath)
+        self.io = open(fpath, 'rb')
+        if self.is_holo:
+            self.io.read(holo_header_size)
 
 
-def parse_title(fpath: str) -> (int, int, int):
+    def get_frame(self) -> bytes:
+        data = []
+        for _ in range(self.height):
+            data.append(self.io.read(self.bytes_per_pixel * self.width))
+        return data
+
+
+def is_holo(fpath: str) -> bool:
+    ext = fpath.split('.')[-1]
+    return ext == "holo"
+
+
+# Returns (img width, img height, bytes per pixel, number of imgs)
+def parse_title(fpath: str) -> (int, int, int, int):
     fname = basename(fpath)
     elems = fname.split('_')
     w, h, nb, _ = elems[(len(elems) - 4):]
     nb = bits_to_bytes[nb]
-    return (int(w), int(h), nb)
+    file_size = getsize(fpath)
+    nb_img = file_size / (int(w) * int(h) * nb)
+    return (int(w), int(h), nb, int(nb_img))
+
+
+# Returns (img width, img height, bytes per pixel, number of imgs)
+def parse_holo(fpath: str) -> (int, int, int, int):
+    with open(fpath, 'rb') as file:
+        header = file.read(holo_header_size)
+        holo, bits_per_pixel, w, h, img_nb = unpack('=4sHIII', header)
+        if holo.decode('ascii') != "HOLO":
+            return (0, 0, 0, 0)
+        return (w, h, int(bits_per_pixel / 8), img_nb)
+
+
+def parse_file(fpath: str) -> FileData:
+    if is_holo(fpath):
+        return FileData(fpath, parse_holo(fpath))
+    else:
+        return FileData(fpath, parse_title(fpath))
--- python/shift.py
+++ python/shift.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-From the first image of a provided RAW file, this builds a RAW file containing
+From the first image of a provided file, this builds a file containing
 10 times the first image, then 10 times that image, shifted by SHIFT pixels.
 """
 
@@ -11,22 +11,23 @@ import holo
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print(f'USAGE: {sys.argv[0]} SHIFT INPUT.raw OUTPUT.raw')
+        print(f'USAGE: {sys.argv[0]} SHIFT INPUT OUTPUT')
 
     shift = int(sys.argv[1])
     input_path = sys.argv[2]
     output_path = sys.argv[3]
 
-    with open(input_path, 'rb') as input_file:
-        data = holo.load_image(input_file, *holo.parse_title(input_path))
-        shifted = []
-        for row in data:
-            shifted.append(row[shift:] + row[:shift])
+    f = holo.parse_file(input_path)
 
-        with open(output_path, 'wb') as output_file:
-            for _ in range(10):
-                for row in data:
-                    output_file.write(row)
-            for _ in range(10):
-                for row in shifted:
-                    output_file.write(row)
+    data = f.get_frame()
+    shifted = []
+    for row in data:
+        shifted.append(row[shift:] + row[:shift])
+
+    with open(output_path, 'wb') as output_file:
+        for _ in range(10):
+            for row in data:
+                output_file.write(row)
+        for _ in range(10):
+            for row in shifted:
+                output_file.write(row)
