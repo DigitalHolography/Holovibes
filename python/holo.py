e27fa0c4b9f36f77f5ff0690a0381058c13718e7
Antoine Martin <antoine4.martin@epita.fr>
2019-11-07 14:09:42 +0100

    up: support new header format in python module

--- python/holo.py
+++ python/holo.py
@@ -4,7 +4,21 @@ from typing import BinaryIO
 from struct import unpack
 
 bits_to_bytes = {'8bit': 1, '16bit': 2}
-holo_header_size = 18
+holo_header_size = 64
+
+struct_format = (
+    '='
+    '4s'
+    'H'                 # unsigned short Version number
+    'H'                 # unsigned short number of bits per pixels
+    'I'                 # unsigned int width of image
+    'I'                 # unsigned int height of image
+    'I'                 # unsigned int number of images
+    'Q'                 # unsigned long long total data size
+    'B'                 # unsigned char endianness
+)
+
+padding_size = 35
 
 class FileData:
     def __init__(self, fpath: str, parse_output: (int, int, int, int)):
@@ -47,7 +61,7 @@ def parse_title(fpath: str) -> (int, int, int, int):
 def parse_holo(fpath: str) -> (int, int, int, int):
     with open(fpath, 'rb') as file:
         header = file.read(holo_header_size)
-        holo, bits_per_pixel, w, h, img_nb = unpack('=4sHIII', header)
+        holo, _version, bits_per_pixel, w, h, img_nb, _data_size, _endianness = unpack(struct_format, header)
         if holo.decode('ascii') != "HOLO":
             return (0, 0, 0, 0)
         return (w, h, int(bits_per_pixel / 8), img_nb)
