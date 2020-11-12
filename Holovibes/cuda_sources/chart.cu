d1be22ca31dee4abf32285ef0fc7697e07609e7e
Quentin Kaci <quentin.kaci@epita.fr>
2020-11-12 10:03:30 +0100

    fix(chart): define atomicAdd for double if CUDA version < 600

--- Holovibes/cuda_sources/chart.cu
+++ Holovibes/cuda_sources/chart.cu
@@ -20,6 +20,30 @@
 #include <cstdio>
 #include <cmath>
 
+// atomicAdd with double is not defined if CUDA Version is not greater than or equal to 600
+// So we use this macro to keep a fully compatible program
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                              (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                               __longlong_as_double(assumed)));
+
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
+
 using holovibes::units::RectFd;
 using holovibes::ChartPoint;
 
