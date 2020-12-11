7825b934c6c81d95aa4acc64022861737a1859ca
Anthony Strazzella <anthony.strazzella@epita.fr>
2020-12-11 16:01:24 +0100

    fix: make FrameReadWorker class as abstract

--- Holovibes/includes/thread/frame_read_worker.hxx
+++ Holovibes/includes/thread/frame_read_worker.hxx
@@ -12,36 +12,10 @@
 
 #pragma once
 
-#include "worker.hh"
-#include "queue.hh"
+#include "frame_read_worker.hh"
 
 namespace holovibes::worker
 {
-    class FrameReadWorker : public Worker
-    {
-    public:
-        FrameReadWorker(std::atomic<std::shared_ptr<Queue>>& gpu_input_queue);
-
-        /*!
-         *  \brief    Abstract destructor to make class abstract
-         */
-        virtual ~FrameReadWorker() = 0;
-
-        /*!
-         *  \brief    Default copy constructor
-         */
-        FrameReadWorker(const FrameReadWorker&) = default;
-
-        /*!
-         *  \brief    Default copy operator
-         */
-        FrameReadWorker& operator=(const FrameReadWorker&) = default;
-
-    protected:
-        std::atomic<std::shared_ptr<Queue>>& gpu_input_queue_;
-
-        std::atomic<unsigned int> processed_fps_;
-    };
-} // namespace holovibes::worker
-
-#include "frame_read_worker.hxx"
+    inline FrameReadWorker::~FrameReadWorker()
+    {}
+} // namespace holovibes::io_files
