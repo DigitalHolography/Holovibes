438cc1e843b6c0988344462f3ee1b3a4e75ea1ce
Ilan Guenet <ilan.guenet@epita.fr>
2020-12-21 10:31:28 +0100

    refactor(build): add comments build.py

--- build.py
+++ build.py
@@ -6,12 +6,12 @@ import sys
 import subprocess
 
 class Config:
-    conf = "Debug"
-    gen = "Ninja"
+    conf = "Debug" # Configuration of the build: debug (default) or relase.
+    gen = "Ninja" # Build system generator: Ninja (default), NMake or Visual Studio (14, 15, 16)
     remain = []
     remain_str = ""
-    build_dir = "build/"
-    vs_version = "Community"
+    build_dir = "build/" # Build directory
+    vs_version = "Community" # Visual studio version: Community (default) or Professional
 
 release_opt = ["Release", "release", "R", "r"]
 debug_opt = ["Debug", "debug", "D", "d"]
@@ -21,6 +21,7 @@ vs_opt = ["Visual Studio 14", "Visual Studio 15", "Visual Studio 16"]
 community_opt = ["Community", "community", "C", "c"]
 professional_opt = ["Professional", "professional", "P", "p"]
 
+# Parse arguments and fill the Config class
 def parse_args(config):
     for i in range(1, len(sys.argv)):
         arg = sys.argv[i]
@@ -44,30 +45,35 @@ def parse_args(config):
             break
     config.build_dir += f"{config.gen}/"
 
+# Logger
 def log(string, arg):
     print(f"[BUILD.PY] {string}: {arg}", flush=True)
 
+# Main function
 if __name__ == "__main__":
     config = Config()
-    parse_args(config)
+    parse_args(config) # Get the build configuration
 
     log("CONFIG", config.conf)
     log("GENERATOR", config.gen)
     log("REMAIN", config.remain)
     log("BUILD_DIR", config.build_dir)
 
-    if "Visual Studio" in config.gen:
+    if "Visual Studio" in config.gen: # If the build system generator is Visual Studio
         if not os.path.isdir(config.build_dir): # create build dir
             cmd = ["cmake", "-G", config.gen, "-B", config.build_dir, "-S", ".", "-A", "x64"]
             log("CMD", cmd)
             subprocess.call(cmd)
+        # Build command
         cmd = ["cmake", "--build", config.build_dir, "--config", config.conf] + config.remain + ["--", "/verbosity:normal"]
         log("CMD", cmd)
         subprocess.call(cmd)
-    else:
+    else: # If the build system generator is Ninja or NMake
+        # Create environment
         cmd = ["cmd.exe", "/c", "call", "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\" + config.vs_version + "\\VC\\Auxiliary\\Build\\vcvars64.bat", "&&"]
         if not os.path.isdir(path.join(config.build_dir, config.conf)): # if build dir doesn't exist, run CMake configure step
             cmd += ["cmake", "-B", config.build_dir, "-S", ".", "-G", config.gen, f"-DCMAKE_BUILD_TYPE={config.conf}", "-DCMAKE_VERBOSE_MAKEFILE=OFF", "&&"]
+        # Build command
         cmd += ["cmake", "--build", config.build_dir] + config.remain
         log("CMD", cmd)
         subprocess.call(cmd)
