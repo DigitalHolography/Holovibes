5f43659e8eff4b62d582e3ceb6562e86a4b2ba3b
Guillaume Poisson <guillaume.poisson@epita.fr>
2021-09-17 19:37:12 +0200

    docs(Doxygen): Normalized comments in includes/core

--- Holovibes/includes/core/icompute.hh
+++ Holovibes/includes/core/icompute.hh
@@ -32,48 +32,41 @@ namespace holovibes
  */
 struct CoreBuffersEnv
 {
-    /** Input buffer. Contains only one frame. We fill it with the input frame*/
-    cuda_tools::UniquePtr<cufftComplex> gpu_spatial_transformation_buffer =
-        nullptr;
+    /*! \brief Input buffer. Contains only one frame. We fill it with the input frame */
+    cuda_tools::UniquePtr<cufftComplex> gpu_spatial_transformation_buffer = nullptr;
 
-    /** Float buffer. Contains only one frame. We fill it with the correct
-     * computed p frame converted to float. */
+    /*! \brief Float buffer. Contains only one frame.
+     *
+     * We fill it with the correct computed p frame converted to float.
+     */
     cuda_tools::UniquePtr<float> gpu_postprocess_frame = nullptr;
-    /** Size in components (size in byte / sizeof(float)) of the
-     gpu_postprocess_frame. Could be removed by changing gpu_postprocess_frame
-     type to cuda_tools::Array. */
+    /*! \brief Size in components (size in byte / sizeof(float)) of the gpu_postprocess_frame.
+     *
+     * Could be removed by changing gpu_postprocess_frame type to cuda_tools::Array.
+     */
     unsigned int gpu_postprocess_frame_size = 0;
-    /** Float XZ buffer. Contains only one frame. We fill it with the correct
-     * computed p XZ frame. */
+    /*! \brief Float XZ buffer of 1 frame, filled with the correct computer p XZ frame. */
     cuda_tools::UniquePtr<float> gpu_postprocess_frame_xz = nullptr;
-    /** Float YZ buffer. Contains only one frame. We fill it with the correct
-     * computed p YZ frame. */
+    /*! \brief Float YZ buffer of 1 frame, filled with the correct computed p YZ frame. */
     cuda_tools::UniquePtr<float> gpu_postprocess_frame_yz = nullptr;
 
-    /** Unsigned Short output buffer. Contains only one frame, inserted after
-     * all postprocessing on float_buffer */
+    /*! \brief Unsigned Short output buffer of 1 frame, inserted after all postprocessing on float_buffer */
     cuda_tools::UniquePtr<unsigned short> gpu_output_frame = nullptr;
-    /** Unsigned Short XZ output buffer. Contains only one frame, inserted after
-     * all postprocessing on float_buffer_cut_xz */
+    /*! \brief Unsigned Short XZ output buffer of 1 frame, inserted after all postprocessing on float_buffer_cut_xz */
     cuda_tools::UniquePtr<unsigned short> gpu_output_frame_xz = nullptr;
-    /** Unsigned Short YZ output buffer. Contains only one frame, inserted after
-     * all postprocessing on float_buffer_cut_yz */
+    /*! \brief Unsigned Short YZ output buffer of 1 frame, inserted after all postprocessing on float_buffer_cut_yz */
     cuda_tools::UniquePtr<unsigned short> gpu_output_frame_yz = nullptr;
 
-    /** Contains only one frame used only for convolution */
+    /*! \brief Contains only one frame used only for convolution */
     cuda_tools::UniquePtr<float> gpu_convolution_buffer = nullptr;
 
-    /** Complex filter2d frame. Contains only one frame. We fill it with the
-     * output_frame */
+    /*! \brief Complex filter2d frame used to store the output_frame */
     cuda_tools::UniquePtr<cufftComplex> gpu_complex_filter2d_frame = nullptr;
-    /** Float Filter2d frame. Contains only one frame. We fill it with the
-     * gpu_complex_filter2d_frame */
+    /*! \brief Float Filter2d frame used to store the gpu_complex_filter2d_frame */
     cuda_tools::UniquePtr<float> gpu_float_filter2d_frame = nullptr;
-    /** Filter2d frame. Contains only one frame. We fill it with the
-     * gpu_float_filter2d_frame */
+    /*! \brief Filter2d frame used to store the gpu_float_filter2d_frame */
     cuda_tools::UniquePtr<unsigned short> gpu_filter2d_frame = nullptr;
-    /** Filter2d mask. Contains only one frame. We apply this mask to
-     * gpu_spatial_transformation_buffer */
+    /*! \brief Filter2d mask applied to gpu_spatial_transformation_buffer */
     cuda_tools::UniquePtr<float> gpu_filter2d_mask = nullptr;
 };
 
@@ -84,15 +77,15 @@ struct CoreBuffersEnv
 struct BatchEnv
 {
     /*! \brief Current frames processed in the batch
-    **
-    ** At index 0, batch_size frames are enqueued, spatial transformation is
-    *also executed in batch
-    ** Batch size frames are enqueued in the gpu_time_transformation_queue
-    ** This is done for perfomances reasons
-    **
-    ** The variable is incremented unil it reachs batch_size in
-    *enqueue_multiple, then it is set back to 0
-    */
+     *
+     * At index 0, batch_size frames are enqueued, spatial transformation is
+     * also executed in batch
+     * Batch size frames are enqueued in the gpu_time_transformation_queue
+     * This is done for perfomances reasons
+     *
+     * The variable is incremented unil it reachs batch_size in
+     *enqueue_multiple, then it is set back to 0
+     */
     uint batch_index = 0;
 };
 
@@ -103,30 +96,41 @@ struct BatchEnv
  */
 struct TimeTransformationEnv
 {
-    /** STFT Queue. Constains time_transformation_size frames. It accumulates
-     input frames after spatial fft,
-     in order to apply STFT only when the frame counter is equal to
-     time_transformation_stride. */
+    /*! \brief STFT Queue. It accumulates input frames after spatial FFT.
+     *
+     * Contains time_transformation_size frames.
+     * Frames are accumulated in order to apply STFT only when
+     * the frame counter is equal to time_transformation_stride.
+     */
     std::unique_ptr<Queue> gpu_time_transformation_queue = nullptr;
-    /** STFT buffer. Contains time_transformation_size frames. Contains the
-     * result of the STFT done on the STFT queue. */
+    /*! \brief STFT buffer.  Contains the result of the STFT done on the STFT queue.
+     *
+     * Contains time_transformation_size frames.
+     */
     cuda_tools::UniquePtr<cufftComplex> gpu_p_acc_buffer = nullptr;
-    /** STFT XZ Queue. Contains the ouput of the STFT on slice XZ. Enqueued with
-     * gpu_float_buffer or gpu_ushort_buffer. */
+    /*! \brief STFT XZ Queue. Contains the ouput of the STFT on slice XZ.
+     *
+     * Enqueued with gpu_float_buffer or gpu_ushort_buffer.
+     */
     std::unique_ptr<Queue> gpu_output_queue_xz = nullptr;
-    /** STFT YZ Queue. Contains the ouput of the STFT on slice YZ. Enqueued with
-     * gpu_float_buffer or gpu_ushort_buffer. */
+    /*! \brief STFT YZ Queue. Contains the ouput of the STFT on slice YZ.
+     *
+     * Enqueued with gpu_float_buffer or gpu_ushort_buffer.
+     */
     std::unique_ptr<Queue> gpu_output_queue_yz = nullptr;
-    /** Plan 1D used for the STFT. */
+    /*! \brief Plan 1D used for the STFT. */
     cuda_tools::CufftHandle stft_plan;
 
-    /** Hold the P frame after the time transformation computation. **/
+    /*! \brief Hold the P frame after the time transformation computation. */
     cuda_tools::UniquePtr<cufftComplex> gpu_p_frame;
 
-    // The following are used for the PCA time transformation
+    /*! \name PCA time transformation
+     * \{
+     */
     cuda_tools::UniquePtr<cuComplex> pca_cov = nullptr;
     cuda_tools::UniquePtr<float> pca_eigen_values = nullptr;
     cuda_tools::UniquePtr<int> pca_dev_info = nullptr;
+    /* \} */
 };
 
 /*! \struct FrameRecordEnv
@@ -157,19 +161,19 @@ struct ChartEnv
  */
 struct ImageAccEnv
 {
-    /// Frame to temporaly store the average on XY view
+    /*! \brief Frame to temporaly store the average on XY view */
     cuda_tools::UniquePtr<float> gpu_float_average_xy_frame = nullptr;
-    /// Queue accumulating the XY computed frames.
+    /*! \brief Queue accumulating the XY computed frames. */
     std::unique_ptr<Queue> gpu_accumulation_xy_queue = nullptr;
 
-    /// Frame to temporaly store the average on XZ view
+    /*! \brief Frame to temporaly store the average on XZ view */
     cuda_tools::UniquePtr<float> gpu_float_average_xz_frame = nullptr;
-    /// Queue accumulating the XZ computed frames.
+    /*! \brief Queue accumulating the XZ computed frames. */
     std::unique_ptr<Queue> gpu_accumulation_xz_queue = nullptr;
 
-    /// Frame to temporaly store the average on YZ axis
+    /*! \brief Frame to temporaly store the average on YZ axis */
     cuda_tools::UniquePtr<float> gpu_float_average_yz_frame = nullptr;
-    /// Queue accumulating the YZ computed frames.
+    /*! \brief Queue accumulating the YZ computed frames. */
     std::unique_ptr<Queue> gpu_accumulation_yz_queue = nullptr;
 };
 
@@ -177,10 +181,10 @@ struct ImageAccEnv
  *
  * \brief Stores functions helping the editing of the images.
  *
- * Stores all the functions that will be used before doing
- * any sort of editing to the image (i.e. refresh functions
- * or caller).
+ * Stores all the functions that will be used before doing any sort
+ * of editing to the image (i.e. refresh functions or caller).
  */
+// #TODO Add \name tags between groups of methods and attributes to make the documentation clearer
 class ICompute : public Observable
 {
     friend class ThreadCompute;
@@ -219,15 +223,14 @@ class ICompute : public Observable
 
     /*! \brief Execute one iteration of the ICompute.
      *
-     * * Checks the number of frames in input queue that must at least
-     * time_transformation_size*.
-     * * Call each function of the ICompute.
-     * * Enqueue the output frame contained in gpu_output_buffer.
-     * * Dequeue one frame of the input queue.
-     * * Check if a ICompute refresh has been requested.
+     * Checks the number of frames in input queue that must at least time_transformation_size.
+     * Call each function of the ICompute.
+     * Enqueue the output frame contained in gpu_output_buffer.
+     * Dequeue one frame of the input queue.
+     * Check if a ICompute refresh has been requested.
      *
-     * The ICompute can not be interrupted for parameters changes until the
-     * refresh method is called. */
+     * The ICompute can not be interrupted for parameters changes until the refresh method is called.
+     */
     virtual void exec() = 0;
 
     void create_stft_slice_queue();
@@ -338,39 +341,42 @@ class ICompute : public Observable
     virtual bool update_time_transformation_size(
         const unsigned short time_transformation_size);
 
-    /* Manage ressources */
+    /*! \name Resources management
+     * \{
+     */
     virtual void update_spatial_transformation_parameters();
     void init_cuts();
     void dispose_cuts();
+    /* \} */
 
     ICompute& operator=(const ICompute&) = delete;
     ICompute(const ICompute&) = delete;
 
   protected:
-    /** Compute Descriptor. */
+    /*! \brief Compute Descriptor. */
     ComputeDescriptor& cd_;
 
-    /** Reference on the input queue, owned by MainWindow. */
+    /*! \brief Reference on the input queue, owned by MainWindow. */
     BatchInputQueue& gpu_input_queue_;
-    /** Reference on the output queue, owned by MainWindow. */
+    /*! \brief Reference on the output queue, owned by MainWindow. */
     Queue& gpu_output_queue_;
 
-    /** Main buffers. */
+    /*! \brief Main buffers. */
     CoreBuffersEnv buffers_;
 
-    /** Batch environment */
+    /*! \brief Batch environment */
     BatchEnv batch_env_;
 
-    /** STFT environment. */
+    /*! \brief STFT environment. */
     TimeTransformationEnv time_transformation_env_;
 
-    /** Frame Record environment (Raw + Hologram)*/
+    /*! \brief Frame Record environment (Raw + Hologram) */
     FrameRecordEnv frame_record_env_;
 
-    /** Chart environment. */
+    /*! \brief Chart environment. */
     ChartEnv chart_env_;
 
-    /** Image accumulation environment */
+    /*! \brief Image accumulation environment */
     ImageAccEnv image_acc_env_;
 
     /*! \brief Queue storing raw frames used by raw view */
@@ -379,21 +385,22 @@ class ICompute : public Observable
     /*! \brief Queue storing filter2d frames */
     std::unique_ptr<Queue> gpu_filter2d_view_queue_{nullptr};
 
-    /** Pland 2D. Used for spatial fft performed on the complex input frame. */
+    /*! \brief Pland 2D. Used for spatial fft performed on the complex input frame. */
     cuda_tools::CufftHandle spatial_transformation_plan_;
 
-    /** Pland 2D. Used for unwrap 2D. */
+    /*! \brief Pland 2D. Used for unwrap 2D. */
     cuda_tools::CufftHandle plan_unwrap_2d_;
 
-    /*! Compute stream to perform  pipe computation */
+    /*! \brief Compute stream to perform pipe computation */
     const cudaStream_t& stream_;
 
-    /** Chrono counting time between two iteration (Taking into account steps,
-     * since it is executing at the end of pipe). */
+    /*! \brief Chrono counting time between two iteration
+     *
+     * Taking into account steps, since it is executing at the end of pipe. 
+     */
     std::chrono::time_point<std::chrono::steady_clock> past_time_;
 
-    /** Counting pipe iteration, in order to update fps only every 100
-     * iterations. */
+    /*! \brief Counting pipe iteration, in order to update fps only every 100 iterations. */
     unsigned int frame_count_{0};
 
     std::atomic<bool> unwrap_1d_requested_{false};
--- Holovibes/includes/core/pipe.hh
+++ Holovibes/includes/core/pipe.hh
@@ -22,7 +22,7 @@ namespace holovibes
  *
  * \brief Pipe is a class that applies processing on input frames.
  *
- * # Why doing this way ?
+ * # Why do it this way ?
  *
  * The goal of the pipe is to build a vector filled with functions to
  * apply on frames. This way it avoids to have a monolithic method plenty of
@@ -55,100 +55,74 @@ class Pipe : public ICompute
 {
   public:
     /*! \brief Allocate CPU/GPU ressources for computation.
+     *
      * \param input Input queue containing acquired frames.
      * \param output Output queue where computed frames will be stored.
      * \param desc ComputeDescriptor that contains computation parameters.
-     * \param stream The compute stream on which all the computations are
-     * processed
+     * \param stream The compute stream on which all the computations are processed
      */
     Pipe(BatchInputQueue& input,
          Queue& output,
          ComputeDescriptor& desc,
          const cudaStream_t& stream);
 
-    /*! \brief Get the lens queue to display it.
-
-    */
+    /*! \brief Get the lens queue to display it. */
     std::unique_ptr<Queue>& get_lens_queue() override;
 
-    /*! \brief Runs a function after the current pipe iteration ends
-
-     */
+    /*! \brief Runs a function after the current pipe iteration ends */
     void insert_fn_end_vect(std::function<void()> function);
-    /*! \brief Calls autocontrast on the *next* pipe iteration on the wanted
-       view
-
-     */
+    /*! \brief Calls autocontrast on the *next* pipe iteration on the wanted view */
     void autocontrast_end_pipe(WindowKind kind);
 
   protected:
     /*! \brief Execute one processing iteration.
      *
-     * * Checks the number of frames in input queue, that must at least
-     * be 1.
-     * * Call each function stored in the FunctionVector.
-     * * Call each function stored in the end FunctionVector, then clears it
-     * * Enqueue the output frame contained in gpu_output_buffer.
-     * * Dequeue one frame of the input queue.
-     * * Check if a ICompute refresh has been requested.
+     * Checks the number of frames in input queue, that must at least be 1.
+     * Call each function stored in the FunctionVector.
+     * Call each function stored in the end FunctionVector, then clears it
+     * Enqueue the output frame contained in gpu_output_buffer.
+     * Dequeue one frame of the input queue.
+     * Check if a ICompute refresh has been requested.
      *
-     * The ICompute can not be interrupted for parameters changes until the
-     * refresh method is called.
-     * If Holovibes crash in a cuda function right after updating something on
-     * the GUI, It probably means that the ComputeDescriptor has been updated
-     * before the end of the iteration. The pipe uses the ComputeDescriptor, and
-     * is refresh only at the end of the iteration. You
-     * **must** wait until the end of the refresh, or use the insert_fn_end_vect
-     * function to update the ComputeDescriptor, otherwise the end of the
-     * current iteration will be wrong, and will maybe crash. */
+     * The ICompute can not be interrupted for parameters changes until the refresh method is called.
+     * If Holovibes crash in a cuda function right after updating something on the GUI, it probably
+     * means thatthe ComputeDescriptor has been updated before the end of the iteration.
+     * The pipe uses the ComputeDescriptor, and is refresh only at the end of the iteration.
+     * You **must** wait until the end of the refresh, or use the insert_fn_end_vect function to update
+     * the ComputeDescriptor, otherwise the end of the current iteration will be wrong, and will maybe crash.
+     */
     virtual void exec();
 
-    /*! \brief Enqueue the main FunctionVector according to the requests.
-
-    */
+    /*! \brief Enqueue the main FunctionVector according to the requests. */
     virtual void refresh();
 
     /*! \brief Make requests at the beginning of the refresh.
+     *
      * Make the allocation of buffers when it is requested.
+     *
      * \return return false if an allocation failed.
      */
     virtual bool make_requests();
 
-    /*!
-    ** \brief Transfer from gpu_space_transformation_buffer to
-    *gpu_time_transformation_queue for time transformation
-    */
+    /*! \brief Transfer from gpu_space_transformation_buffer to gpu_time_transformation_queue for time transform */
     void insert_transfer_for_time_transformation();
 
-    /*!
-    ** \brief Increase batch_index by batch_size
-    */
+    /*! \brief Increase batch_index by batch_size */
     void update_batch_index();
 
-    /*!
-    ** \brief Wait that there are at least a batch of frames in input queue
-    */
+    /*! \brief Wait that there are at least a batch of frames in input queue */
     void insert_wait_frames();
 
-    /*!
-    ** \brief Dequeue the input queue frame by frame in raw mode
-    */
+    /*! \brief Dequeue the input queue frame by frame in raw mode */
     void insert_dequeue_input();
 
-    /*!
-    ** \brief Enqueue the output frame in the output queue in hologram mode
-    */
+    /*! \brief Enqueue the output frame in the output queue in hologram mode */
     void insert_output_enqueue_hologram_mode();
 
-    /*!
-    ** \brief Enqueue the output frame in the filter2d view queue
-    */
+    /*! \brief Enqueue the output frame in the filter2d view queue */
     void insert_filter2d_view();
 
-    /*!
-    ** \brief Request the computation of a autocontrast if the contrast and
-    ** the contrast refresh is enabled
-    */
+    /*! \brief Request the computation of a autocontrast if the contrast and the contrast refresh is enabled */
     void insert_request_autocontrast();
 
     void insert_raw_view();
@@ -157,34 +131,30 @@ class Pipe : public ICompute
 
     void insert_hologram_record();
 
-    /*!
-    ** \brief Reset the batch index if time_transformation_stride has been
-    *reached
-    */
+    /*! \brief Reset the batch index if time_transformation_stride has been reached */
     void insert_reset_batch_index();
 
-    /*!
-    ** \brief Enqueue a frame in an output queue
-    **
-    ** \param output_queue Queue in which the frame is enqueued
-    ** \param frame Frame to enqueue
-    ** \param error Error message when an error occurs
-    */
+    /*! \brief Enqueue a frame in an output queue
+     *
+     * \param output_queue Queue in which the frame is enqueued
+     * \param frame Frame to enqueue
+     * \param error Error message when an error occurs
+     */
     void safe_enqueue_output(Queue& output_queue,
                              unsigned short* frame,
                              const std::string& error);
 
   private:
-    //! Vector of functions that will be executed in the exec() function.
+    /*! \brief Vector of functions that will be executed in the exec() function. */
     FunctionVector fn_compute_vect_;
 
-    //! Vecor of functions that will be executed once, after the execution of
-    //! fn_compute_vect_.
+    /*! \brief Vecor of functions that will be executed once, after the execution of fn_compute_vect_. */
     FunctionVector fn_end_vect_;
-    /*! Mutex that prevents the insertion of a function during its execution.
-        Since we can insert functions in fn_end_vect_ from other threads
-       (MainWindow), we need to lock it.
-    */
+
+    /*! \brief Mutex that prevents the insertion of a function during its execution.
+     *
+     * Since we can insert functions in fn_end_vect_ from other threads  MainWindow), we need to lock it.
+     */
     std::mutex fn_end_vect_mutex_;
 
     std::unique_ptr<compute::ImageAccumulation> image_accumulation_;
@@ -196,9 +166,9 @@ class Pipe : public ICompute
     std::atomic<unsigned int> processed_output_fps_;
 
     /*! \brief Iterates and executes function of the pipe.
-
-      It will first iterate over fn_compute_vect_, then over function_end_pipe_.
-    */
+     *
+     * It will first iterate over fn_compute_vect_, then over function_end_pipe_.
+     */
     void run_all();
 };
 } // namespace holovibes
