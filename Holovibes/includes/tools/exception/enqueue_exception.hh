60eabb18f9d6872684ff8d23f86b7dff078b1c26
fabien.colmagro <fabien.colmagro@epita.fr>
2021-09-17 16:42:36 +0200

    doc(exception): doxygen comments added

--- Holovibes/includes/tools/exception/enqueue_exception.hh
+++ Holovibes/includes/tools/exception/enqueue_exception.hh
@@ -12,23 +12,38 @@
 
 namespace holovibes
 {
-/*! \class EnqueueException
- *
- * \brief #TODO Add a description for this class
+/*!
+ * \class EnqueueException
  */
 class EnqueueException : public CustomException
 {
   public:
+    /*!
+     * \brief Construct a new Enqueue Exception object
+     *
+     * \param msg the message you want to display with the exception
+     */
     EnqueueException(const std::string& msg)
         : CustomException(msg.c_str())
     {
     }
 
+    /*!
+     * \brief Construct a new Enqueue Exception object
+     *
+     * \param msg the message you want to display with the exception
+     * \param line the line from which the exception is triggered (__LINE__ in macro ONLY)
+     * \param file the file from which the exception is triggered (__FILE__ in macro ONLY)
+     */
     EnqueueException(const std::string& msg, const int line, const char* file)
         : CustomException(msg.c_str(), line, file)
     {
     }
 
+    /*!
+     * \brief Destroy the Enqueue Exception object
+     *
+     */
     ~EnqueueException() {}
 };
 } // namespace holovibes
