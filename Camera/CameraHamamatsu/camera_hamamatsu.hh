1d576dc5a6f9d5bf1e8b48a50fffbe4a7fdd3805
Guillaume Poisson <guillaume.poisson@epita.fr>
2021-09-20 21:21:50 +0200

    docs(Doxygen): Normalized comments in Camera/

--- Camera/CameraHamamatsu/camera_hamamatsu.hh
+++ Camera/CameraHamamatsu/camera_hamamatsu.hh
@@ -40,28 +40,41 @@ class CameraHamamatsu : public Camera
 
     std::unique_ptr<unsigned short[]> output_frame_;
 
-    HDCAM hdcam_;     // Camera handle
-    HDCAMWAIT hwait_; // Event waiter handle (will typically wait for the
-                      // FRAME_READY event)
+    /*! \brief Camera Handle */
+    HDCAM hdcam_;
+    /*! \brief Event waiter handle (will typically wait for the FRAME_READY event) */
+    HDCAMWAIT hwait_;
+
     /*! \brief start position x axis of the region of interest */
     long srcox_;
     /*! \brief start position y axis of the region of interest */
     long srcoy_;
-    unsigned short binning_; // 1, 2, 4
+    /*! \brief 1, 2, 4 */
+    unsigned short binning_;
     bool ext_trig_;
     int32 circ_buffer_frame_count_;
-    _DCAMPROPMODEVALUE trig_mode_;      // NORMAL, START
-    _DCAMPROPMODEVALUE trig_connector_; // INTERFACE, BNC
-    _DCAMPROPMODEVALUE trig_polarity_;  // POSITIVE, NEGATIVE
-    _DCAMPROPMODEVALUE readoutspeed_;   // SLOWEST, FASTEST
-    _DCAMPROPMODEVALUE trig_active_;    // EDGE, LEVEL,
+    /*! \brief NORMAL, START */
+    _DCAMPROPMODEVALUE trig_mode_;
+    /*! \brief INTERFACE, BNC */
+    _DCAMPROPMODEVALUE trig_connector_;
+    /*! \brief POSITIVE, NEGATIVE */
+    _DCAMPROPMODEVALUE trig_polarity_;
+    /*! \brief SLOWEST, FASTEST */
+    _DCAMPROPMODEVALUE readoutspeed_;
+    /*! \brief EDGE, LEVEL */
+    _DCAMPROPMODEVALUE trig_active_;
 
     static constexpr unsigned short MAX_WIDTH = 2304;
     static constexpr unsigned short MAX_HEIGHT = 2304;
 
-    // Structures needed to acquire frames
-    // Will be passed as arguments to the API functions
+    /*! \name Frame aquiring
+     * \{
+     *
+     * Structures needed to acquire frames
+     * Will be passed as arguments to the API functions
+     */
     DCAMBUF_FRAME dcam_frame_acq_info_;
     DCAMWAIT_START dcam_wait_info_;
+    /*! \} */
 };
 } // namespace camera
