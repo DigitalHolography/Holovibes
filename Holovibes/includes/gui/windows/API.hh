3504d725665109e256133ee7b16c5fe1f861cdf0
fabien.colmagro <fabien.colmagro@epita.fr>
2021-09-28 14:57:47 +0200

    doc(API.hh): comments added

--- Holovibes/includes/gui/windows/API.hh
+++ Holovibes/includes/gui/windows/API.hh
@@ -8,8 +8,28 @@
 
 namespace holovibes::api
 {
+/*! \brief Gets an Input file from a given filename
+ *
+ * \param filename the given filename to open
+ * \return std::optional<::holovibes::io_files::InputFrameFile*> the file on success, nullopt on error
+ */
 std::optional<::holovibes::io_files::InputFrameFile*> import_file(const std::string& filename);
 
+/*!
+ * \brief Launchs the reading of a given inputed file
+ *
+ * \param mainwindow TODO: to remove
+ * \param holovibes the object used for launching
+ * \param file_fd TODO: to store in an object
+ * \param is_enabled_camera TODO: to store in an object
+ * \param file_path
+ * \param fps
+ * \param first_frame
+ * \param load_file_in_gpu
+ * \param last_frame
+ * \return true on success
+ * \return false on failure
+ */
 bool import_start(::holovibes::gui::MainWindow& mainwindow,
                   Holovibes& holovibes,
                   camera::FrameDescriptor& file_fd,
@@ -20,7 +40,20 @@ bool import_start(::holovibes::gui::MainWindow& mainwindow,
                   bool load_file_in_gpu,
                   size_t last_frame);
 
+/*!
+ * \brief Stops the display
+ *
+ * \param mainwindow TODO: to remove
+ * \param holovibes the object embeding the display to stop
+ */
 void import_stop(::holovibes::gui::MainWindow& mainwindow, Holovibes& holovibes);
+
+/*!
+ * \brief Stops the program compute
+ *
+ * \param mainwindow TODO: to remove
+ * \param holovibes the computing object
+ */
 void close_critical_compute(::holovibes::gui::MainWindow& mainwindow, Holovibes& holovibes);
 
 } // namespace holovibes::api
