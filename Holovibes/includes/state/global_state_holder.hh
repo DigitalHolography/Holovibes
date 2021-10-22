fbbe077f8d2310f0b8bcfbd697202d9cdededaad
Marius <mariusdubosc@gmail.com>
2021-10-22 15:41:22 +0200

    doc(state): adding documentation to the global state holder and the micro-cache

--- Holovibes/includes/state/global_state_holder.hh
+++ Holovibes/includes/state/global_state_holder.hh
@@ -7,6 +7,27 @@
 namespace holovibes
 {
 
+/*!
+ * \brief The GSH (global state holder), is where all the global state of the program is stored. Its goal is to register
+ * changes commanded by the API (which comes from user events), dispatch the changes to each worker, and provide
+ * informations queried by the API. It relies on several structures and mecanisms :
+ *
+ * Queries and Commands : every data needed by the API shall be obtained in the form of structured Queries, provided by
+ * the GSH. This guarantees that only needed data is accessed to, and grants better code readability.
+ * The same principle is applied to changes comming from the API, which come in the form of structured Commands.
+ *
+ * MicroCache : local state holder belonging to each worker. Previously, each worker had to fetch data at the same
+ * place ; therefore, all variables had to be atomic, with the aim to be thread safe. In order to avoid that, each
+ * worker now possess MicroCaches, containing all the state they need. Those MicroCaches are accessed only by their
+ * worker, and are synchronized with the GSH when each worker chooses to (using a trigger system).
+ * More informations and explanations concerning their synchronization with the GSH are provided in files micro-cache.hh
+ * and micro-cache.hxx.
+ *
+ * FastUpdateHolder : the fastUpdateHolder is a templated map which is used by the informationWorker to access and
+ * display informations (like fps and queue occupancy) at a high rate, since those datas need to be updated
+ * continuously.
+ *
+ */
 class GSH
 {
   public:
--- Holovibes/includes/state/micro_cache.hxx
+++ Holovibes/includes/state/micro_cache.hxx
@@ -2,6 +2,52 @@
 
 #include "micro_cache.hh"
 
+/*!
+ * \brief This macro is the core of the micro-cache's functionning.
+ * As explained in micro_cache.hh, the microcache needs to regularly synchronize with the GSH. In order to know which
+ * variable(s) have changed during synchronization, we store the variables in a struct whose name is the variable's
+ * name, and we generate a setter at compile time.
+ * This setter is used in the commands received by the GSH ; while updating it's own parameters, it will also store the
+ * new values in every to_update field of the corresponding micro-caches variables.
+ *
+ * For example, consider this simple struct ExampleCache:
+ *
+ *  struct ExampleCache : public MicroCache
+ *  {
+ *      MONITORED_MEMBER(int, a)
+ *  };
+ *
+ * At compile time, it will expand to:
+ *
+ *  struct ExampleCache : public MicroCache
+ *  {
+ *    private:
+ *      struct a_t
+ *      {
+ *          int obj;
+ *          int* volatile to_update = nullptr;
+ *      };
+ *      a_t a;
+ *
+ *      void set_a(int _val)
+ *      {
+ *          a.obj = _val;
+ *          for (MicroCache * cache : micro_caches_)
+ *          {
+ *              decltype(this) underlying_cache = dynamic_cast<decltype(this)>(cache);
+ *              if (underlying_cache == nullptr)
+ *                  return;
+ *
+ *              underlying_cache->a.to_update = &a.obj;
+ *          }
+ *      }
+ *
+ *    public: int get_a() { return a.obj; }
+ *  };
+ *
+ *
+ */
+
 #define MONITORED_MEMBER(type, var)                                                                                    \
   private:                                                                                                             \
     struct var##_t                                                                                                     \
