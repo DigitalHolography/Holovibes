36f2f64f1f57192e63537e3c9063a3d467f89d73
Eliott Bouhana <eliottmini@gmail.com>
2021-11-01 13:33:11 +0100

    feat(concepts): Added MicroCacheDerived concept

--- Holovibes/includes/state/micro_cache.hh
+++ Holovibes/includes/state/micro_cache.hh
@@ -3,6 +3,11 @@
 namespace holovibes
 {
 
+struct MicroCache;
+
+template <class T>
+concept MicroCacheDerived = std::is_base_of<MicroCache, std::remove_reference_t<T>>::value;
+
 struct MicroCache
 {
     MicroCache(bool truth);
@@ -11,10 +16,10 @@ struct MicroCache
 
   protected:
     const bool truth_;
-    template <class T>
+    template <MicroCacheDerived T>
     static inline std::remove_reference_t<T>* cache_truth;
 
-    template <class T>
+    template <MicroCacheDerived T>
     static inline std::set<std::remove_reference_t<T>*> micro_caches;
 };
 } // namespace holovibes
