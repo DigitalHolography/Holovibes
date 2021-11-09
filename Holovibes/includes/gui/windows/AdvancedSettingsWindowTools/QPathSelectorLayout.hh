3536872be500ef5f99047545d49c18509312accf
fabien.colmagro <fabien.colmagro@epita.fr>
2021-11-09 11:28:20 +0100

    docs(AdvancedSettingsWindow): comments added over path selector tool

--- Holovibes/includes/gui/windows/AdvancedSettingsWindowTools/QPathSelectorLayout.hh
+++ Holovibes/includes/gui/windows/AdvancedSettingsWindowTools/QPathSelectorLayout.hh
@@ -1,3 +1,7 @@
+/*! \file
+ *
+ * \brief Qt widget embeded into layout.
+ */
 #pragma once
 
 #include <QHBoxLayout>
@@ -7,17 +11,40 @@
 
 namespace holovibes::gui
 {
+/*! \class QPathSelectorLayout
+ *
+ * \brief Enhancement of path selector system display.
+ */
 class QPathSelectorLayout : public QHBoxLayout
 {
     Q_OBJECT
   public:
+    /*! \brief QPathSelectorLayout object constructor
+     *
+     * \param parent the object that will embed the layouts
+     * \param parent_widget the object that will embed the object
+     */
     QPathSelectorLayout(QMainWindow* parent = nullptr, QWidget* parent_widget = nullptr);
+
+    /*! \brief QPathSelectorLayout object destructor */
     ~QPathSelectorLayout();
 
+    /*! \brief Sets the selected path into the line edit box
+     *
+     * \param text the new text
+     * \return QPathSelectorLayout* this, for linked initilizer purposes
+     */
     QPathSelectorLayout* setText(const std::string& text);
+
+    /*! \brief Sets the name/label of the layout
+     *
+     * \param name the new name
+     * \return QPathSelectorLayout* this, for linked initilizer purposes
+     */
     QPathSelectorLayout* setName(const std::string& name);
 
   private slots:
+    /*! \brief Calls on new path selection*/
     void change_folder();
 
   private:
