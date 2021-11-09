2815e10c39455e4917dcd1d98aa1eaf292cb1ad5
fabien.colmagro <fabien.colmagro@epita.fr>
2021-11-09 17:46:27 +0100

    refacto(ASWPanelChart): the qt signals now climb the inheritance from bottom (sender) to top (processor)

--- Holovibes/sources/gui/windows/AdvancedSettingsWindowTools/panels/asw_panel_chart.cc
+++ Holovibes/sources/gui/windows/AdvancedSettingsWindowTools/panels/asw_panel_chart.cc
@@ -12,10 +12,20 @@ ASWPanelChart::ASWPanelChart(QMainWindow* parent, QWidget* parent_widget)
     auto_scale_point_threshold_ = new QIntSpinBoxLayout(parent, parent_widget, "DisplayRate");
     auto_scale_point_threshold_->setValue(100);
     chart_layout_->addItem(auto_scale_point_threshold_);
+    connect(auto_scale_point_threshold_,
+            SIGNAL(value_changed()),
+            this,
+            SLOT(on_change_auto_scale_point_threshold_value()));
 
     setLayout(chart_layout_);
 }
 
 ASWPanelChart::~ASWPanelChart() {}
+
+#pragma region SLOTS
+// TODO: region to implement with API
+void ASWPanelChart::on_change_auto_scale_point_threshold_value() { LOG_INFO; }
+
+#pragma endregion
 
 } // namespace holovibes::gui
