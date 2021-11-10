62113aa26cd3099813348981cd46123e24b86932
fabien.colmagro <fabien.colmagro@epita.fr>
2021-11-10 17:07:15 +0100

    refacto(ASWPanel*): each panel modifies the backend values through api when slot is triggered

--- Holovibes/sources/gui/windows/AdvancedSettingsWindowTools/panels/asw_panel_advanced.cc
+++ Holovibes/sources/gui/windows/AdvancedSettingsWindowTools/panels/asw_panel_advanced.cc
@@ -66,7 +66,7 @@ void ASWPanelAdvanced::create_contrast_lower_threshold_widget()
 {
     // Contrast lower threshold spin box
     contrast_lower_threshold_ = new QDoubleSpinBoxLayout(parent_widget_, "Contrast_lower_threshold");
-    contrast_lower_threshold_->setValue(DEFAULT_CONTRAST_UPPER_THRESHOLD_VALUE);
+    contrast_lower_threshold_->setValue(DEFAULT_CONTRAST_LOWER_THRESHOLD_VALUE);
     advanced_layout_->addItem(contrast_lower_threshold_);
     connect(contrast_lower_threshold_, SIGNAL(value_changed()), this, SLOT(on_change_contrast_lower_threshold_value()));
 }
@@ -101,25 +101,47 @@ void ASWPanelAdvanced::create_cuts_contrast_p_offset_widget()
 
 #pragma region SLOTS
 // TODO: region to implement with API
-void ASWPanelAdvanced::on_change_display_rate_value() { LOG_INFO << display_rate_->get_value(); };
+void ASWPanelAdvanced::on_change_display_rate_value()
+{
+    LOG_INFO << display_rate_->get_value();
+    api::set_display_rate(display_rate_->get_value());
+}
 
-void ASWPanelAdvanced::on_change_filter2d_smooth_low_value() { LOG_INFO << filter2d_smooth_low_->get_value(); };
+void ASWPanelAdvanced::on_change_filter2d_smooth_low_value()
+{
+    LOG_INFO << filter2d_smooth_low_->get_value();
+    api::set_filter2d_smooth_low(filter2d_smooth_low_->get_value());
+}
 
-void ASWPanelAdvanced::on_change_filter2d_smooth_high_value() { LOG_INFO << filter2d_smooth_high_->get_value(); };
+void ASWPanelAdvanced::on_change_filter2d_smooth_high_value()
+{
+    LOG_INFO << filter2d_smooth_high_->get_value();
+    api::set_filter2d_smooth_high(filter2d_smooth_high_->get_value());
+}
 
 void ASWPanelAdvanced::on_change_contrast_lower_threshold_value()
 {
     LOG_INFO << contrast_lower_threshold_->get_value();
-};
+    api::set_contrast_lower_threshold(contrast_lower_threshold_->get_value());
+}
 
 void ASWPanelAdvanced::on_change_contrast_upper_threshold_value()
 {
     LOG_INFO << contrast_upper_threshold_->get_value();
-};
+    api::set_contrast_upper_threshold(contrast_upper_threshold_->get_value());
+}
 
-void ASWPanelAdvanced::on_change_renorm_constant_value() { LOG_INFO << renorm_constant_->get_value(); };
+void ASWPanelAdvanced::on_change_renorm_constant_value()
+{
+    LOG_INFO << renorm_constant_->get_value();
+    api::set_renorm_constant(renorm_constant_->get_value());
+}
 
-void ASWPanelAdvanced::on_change_cuts_contrast_p_offset_value() { LOG_INFO << cuts_contrast_p_offset_->get_value(); };
+void ASWPanelAdvanced::on_change_cuts_contrast_p_offset_value()
+{
+    LOG_INFO << cuts_contrast_p_offset_->get_value();
+    api::set_cuts_contrast_p_offset(cuts_contrast_p_offset_->get_value());
+}
 #pragma endregion
 
 } // namespace holovibes::gui
