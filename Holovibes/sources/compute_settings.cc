6f58b04ccac4bc0db5e6822d117f45ea42470e3e
philippe.bernet <philippe.bernet@epita.fr>
2021-11-29 15:30:45 +0100

    clean(compute_settigns): remove dead code

--- Holovibes/sources/compute_settings.cc
+++ Holovibes/sources/compute_settings.cc
@@ -2,183 +2,6 @@
 
 namespace holovibes::api
 {
-void load_image_rendering(const boost::property_tree::ptree& ptree, ComputeDescriptor& cd)
-{
-    set_compute_mode(static_cast<Computation>(
-        ptree.get<int>("image_rendering.image_mode", static_cast<int>(cd.compute_mode.load()))));
-
-    set_batch_size(ptree.get<ushort>("image_rendering.batch_size", cd.batch_size));
-    set_time_transformation_stride(
-        ptree.get<ushort>("image_rendering.time_transformation_stride", cd.time_transformation_stride));
-
-    set_filter2d(ptree.get<bool>("image_rendering.filter2d_enabled", cd.filter2d_enabled));
-    set_filter2d_n1(ptree.get<int>("image_rendering.filter2d_n1", cd.filter2d_n1));
-    set_filter2d_n2(ptree.get<int>("image_rendering.filter2d_n2", cd.filter2d_n2));
-
-    set_space_transformation(static_cast<SpaceTransformation>(
-        ptree.get<int>("image_rendering.space_transformation", static_cast<int>(cd.space_transformation.load()))));
-    set_time_transformation(static_cast<TimeTransformation>(
-        ptree.get<int>("image_rendering.time_transformation", static_cast<int>(cd.time_transformation.load()))));
-
-    set_time_transformation_size(
-        ptree.get<ushort>("image_rendering.time_transformation_size", cd.time_transformation_size));
-
-    set_wavelength(ptree.get<float>("image_rendering.lambda", cd.lambda));
-    set_z_distance(ptree.get<float>("image_rendering.z_distance", cd.zdistance));
-
-    ////// TODO: Remove checkboxe ??
-    ////// TODO: Think about how to store the type. Some new convolutions type might be added in AppData
-    ////// set_convolution_enabled(ptree.get<bool>("image_rendering.convolution_enabled", cd.convolution_enabled));
-    ////// cd.convolution_type( ptree.get("image_rendering.convolution_type", cd.convolution_enabled));
-    set_divide_convolution(
-        ptree.get<bool>("image_rendering.divide_convolution_enabled", cd.divide_convolution_enabled));
-}
-
-void load_view(const boost::property_tree::ptree& ptree, ComputeDescriptor& cd)
-{
-    set_img_type(static_cast<ImgType>(ptree.get<int>("view.view_type", static_cast<int>(cd.img_type.load()))));
-    // Add unwrap_2d
-    set_fft_shift(ptree.get<bool>("view.fft_shift_enabled", cd.fft_shift_enabled));
-
-    auto xypq_load = [&](const std::string name, View_Accu& view)
-    { view.accu_level = ptree.get<short>("view." + name + "_accu_level", view.accu_level); };
-    auto xy_load = [&](const std::string name, View_XY& view)
-    {
-        view.cuts = ptree.get<ushort>("view." + name + "_cuts", view.cuts);
-        xypq_load(name, view);
-    };
-    auto pq_load = [&](const std::string name, View_PQ& view)
-    {
-        view.index = ptree.get<ushort>("view." + name + "_index", view.index);
-        xypq_load(name, view);
-    };
-
-    xy_load("x", cd.x);
-    xy_load("y", cd.y);
-    pq_load("p", cd.p);
-    pq_load("q", cd.q);
-
-    auto xyzf_load = [&](const std::string name, View_Window& view)
-    {
-        view.log_scale_slice_enabled =
-            ptree.get<bool>("view." + name + "_log_scale_enabled", view.log_scale_slice_enabled);
-
-        view.contrast_enabled = ptree.get<bool>("view." + name + "_contrast_enabled", view.contrast_enabled);
-        view.contrast_auto_refresh =
-            ptree.get<bool>("view." + name + "_auto_contrast_enabled", view.contrast_auto_refresh);
-        view.contrast_invert = ptree.get<bool>("view." + name + "_invert_enabled", view.contrast_invert);
-        view.contrast_min = ptree.get<float>("view." + name + "_contrast_min", view.contrast_min);
-        view.contrast_max = ptree.get<float>("view." + name + "_contrast_max", view.contrast_max);
-    };
-
-    auto xyz_load = [&](const std::string name, View_XYZ& view)
-    {
-        view.flip_enabled = ptree.get<bool>("view." + name + "_flip_enabled", view.flip_enabled);
-        view.rot = ptree.get<float>("view." + name + "_rot", view.rot);
-
-        view.img_accu_level = ptree.get<ushort>("view." + name + "_img_accu_level", view.img_accu_level);
-
-        xyzf_load(name, view);
-    };
-
-    xyz_load("xy", cd.xy);
-    xyz_load("xz", cd.xz);
-    xyz_load("yz", cd.yz);
-    xyzf_load("filter2d", cd.filter2d);
-
-    toggle_renormalize(ptree.get<bool>("view.renorm_enabled", cd.renorm_enabled));
-
-    display_reticle(ptree.get<bool>("view.reticle_display_enabled", cd.reticle_display_enabled));
-    reticle_scale(ptree.get<float>("view.reticle_scale", cd.reticle_scale));
-}
-
-void load_composite(const boost::property_tree::ptree& ptree, ComputeDescriptor& cd)
-{
-    set_composite_kind(
-        static_cast<CompositeKind>(ptree.get<int>("composite.mode", static_cast<int>(cd.composite_kind.load()))));
-    set_composite_auto_weights(ptree.get<bool>("composite.auto_weights_enabled", cd.composite_auto_weights));
-
-    auto p_load = [&](const std::string& name, Composite_P& p)
-    {
-        p.p_min = ptree.get<ushort>("composite." + name + "_p_min", cd.rgb.p_min);
-        p.p_max = ptree.get<ushort>("composite." + name + "_p_max", cd.rgb.p_max);
-    };
-
-    p_load("rgb", cd.rgb);
-    cd.rgb.weight_r = ptree.get<float>("composite.rgb_weight_r", cd.rgb.weight_r);
-    cd.rgb.weight_g = ptree.get<float>("composite.rgb_weight_g", cd.rgb.weight_g);
-    cd.rgb.weight_b = ptree.get<float>("composite.rgb_weight_b", cd.rgb.weight_b);
-
-    auto hsv_load = [&](const std::string& name, Composite_hsv& s)
-    {
-        p_load(name, s);
-        s.slider_threshold_min = ptree.get<float>("composite." + name + "_min_value", s.slider_threshold_min);
-        s.slider_threshold_max = ptree.get<float>("composite." + name + "_max_value", s.slider_threshold_max);
-        s.low_threshold = ptree.get<float>("composite." + name + "_low_threshold", s.low_threshold);
-        s.high_threshold = ptree.get<float>("composite." + name + "_high_threshold", s.high_threshold);
-    };
-
-    hsv_load("hsv_h", cd.hsv.h);
-    cd.hsv.h.blur_enabled = ptree.get<bool>("hsv_h.blur_enabled", cd.hsv.h.blur_enabled);
-    cd.hsv.h.blur_kernel_size = ptree.get<uint>("hsv_h.blur_size", cd.hsv.h.blur_kernel_size);
-
-    auto sv_load = [&](const std::string& name, Composite_SV& s)
-    {
-        s.p_activated = ptree.get<bool>("composite." + name + "_enabled", s.p_activated);
-        hsv_load(name, s);
-    };
-
-    sv_load("hsv_s", cd.hsv.s);
-    sv_load("hsv_v", cd.hsv.v);
-}
-
-void load_advanced(const boost::property_tree::ptree& ptree, ComputeDescriptor& cd)
-{
-    cd.file_buffer_size = ptree.get<ushort>("advanced.file_buffer_size", cd.file_buffer_size);
-    cd.input_buffer_size = ptree.get<ushort>("advanced.input_buffer_size", cd.input_buffer_size);
-    cd.record_buffer_size = ptree.get<ushort>("advanced.record_buffer_size", cd.record_buffer_size);
-    cd.output_buffer_size = ptree.get<ushort>("advanced.output_buffer_size", cd.output_buffer_size);
-    cd.time_transformation_cuts_output_buffer_size =
-        ptree.get<ushort>("advanced.time_transformation_cuts_output_buffer_size",
-                          cd.time_transformation_cuts_output_buffer_size);
-
-    cd.display_rate = ptree.get<float>("advanced.display_rate", cd.display_rate);
-    cd.filter2d_smooth_low = ptree.get<int>("advanced.filter2d_smooth_low", cd.filter2d_smooth_low);
-    cd.filter2d_smooth_high = ptree.get<int>("advanced.filter2d_smooth_high", cd.filter2d_smooth_high);
-    cd.contrast_lower_threshold = ptree.get<float>("advanced.contrast_lower_threshold", cd.contrast_lower_threshold);
-    cd.contrast_upper_threshold = ptree.get<float>("advanced.contrast_upper_threshold", cd.contrast_upper_threshold);
-    cd.renorm_constant = ptree.get<uint>("advanced.renorm_constant", cd.renorm_constant);
-    cd.cuts_contrast_p_offset = ptree.get<ushort>("view.cuts_contrast_p_offset", cd.cuts_contrast_p_offset);
-}
-
-void load_view_visibility(const boost::property_tree::ptree& ptree, ComputeDescriptor& cd)
-{
-    // May just not be in compute_settings.ini because those are settings the user will be able to see
-    // if it checked the relevent checbox in the UI.
-    // Sets directly using cd because no need of the intern checks of the api function
-
-    get_cd().set_filter2d_view_enabled(
-        ptree.get<bool>("image_rendering.filter2d_view_enabled", cd.filter2d_view_enabled));
-    get_cd().set_3d_cuts_view_enabled(ptree.get<bool>("view.3d_cuts_enabled", cd.time_transformation_cuts_enabled));
-    get_cd().set_lens_view_enabled(ptree.get<bool>("view.lens_view_enabled", cd.lens_view_enabled));
-    get_cd().set_raw_view_enabled(ptree.get<bool>("view.raw_view_enabled", cd.raw_view_enabled));
-}
-
-void after_load_checks(ComputeDescriptor& cd)
-{
-    if (cd.filter2d_n1 >= cd.filter2d_n2)
-        cd.filter2d_n1 = cd.filter2d_n2 - 1;
-    if (cd.time_transformation_size < 1)
-        cd.time_transformation_size = 1;
-    // TODO: Check convolution type if it  exists (when it will be added to cd)
-    if (cd.p.index >= cd.time_transformation_size)
-        cd.p.index = 0;
-    if (cd.q.index >= cd.time_transformation_size)
-        cd.q.index = 0;
-    if (cd.cuts_contrast_p_offset > cd.time_transformation_size - 1)
-        cd.cuts_contrast_p_offset = cd.time_transformation_size - 1;
-}
-
 void load_image_rendering(const json& data)
 {
     ComputeDescriptor& cd = api::get_cd();
@@ -267,49 +90,34 @@ void json_to_compute_settings(const json& data)
     load_advanced(data["advanced"]);
 }
 
+void after_load_checks(ComputeDescriptor& cd)
+{
+    if (cd.filter2d_n1 >= cd.filter2d_n2)
+        cd.filter2d_n1 = cd.filter2d_n2 - 1;
+    if (cd.time_transformation_size < 1)
+        cd.time_transformation_size = 1;
+    // TODO: Check convolution type if it  exists (when it will be added to cd)
+    if (cd.p.index >= cd.time_transformation_size)
+        cd.p.index = 0;
+    if (cd.q.index >= cd.time_transformation_size)
+        cd.q.index = 0;
+    if (cd.cuts_contrast_p_offset > cd.time_transformation_size - 1)
+        cd.cuts_contrast_p_offset = cd.time_transformation_size - 1;
+}
+
 void load_compute_settings(const std::string& json_path)
 {
     if (json_path.empty())
         return;
 
-    if (json_path.ends_with(".json"))
-    {
+    LOG_INFO << "Compute settings loaded from : " << json_path;
 
-        LOG_INFO << "Compute settings loaded from : " << json_path;
+    std::ifstream ifs(json_path);
+    auto j_cs = json::parse(ifs);
 
-        std::ifstream ifs(json_path);
-        auto j_cs = json::parse(ifs);
-
-        try
-        {
-            json_to_compute_settings(j_cs);
-        }
-        catch (json::exception&)
-        {
-            LOG_ERROR << "Bad format in file : " << json_path;
-        }
-    }
-    else if (json_path.ends_with(".ini"))
-    {
-
-        boost::property_tree::ptree ptree;
-        boost::property_tree::ini_parser::read_ini(json_path, ptree);
-        
-        load_image_rendering(ptree, get_cd());
-        load_view(ptree, get_cd());
-        load_composite(ptree, get_cd());
-        load_advanced(ptree, get_cd());
-
-        save_compute_settings(json_path);
-    }
-
-    // Currently not working.
-    // The app crash when one of the visibility is already set at when the app begins.
-    // Possible problem: Concurrency between maindisplay and the other displays
-    // load_view_visibility(ptree, get_cd());
+    json_to_compute_settings(j_cs);
 
     after_load_checks(get_cd());
-
     pipe_refresh();
 }
 
