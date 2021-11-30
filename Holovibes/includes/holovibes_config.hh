3f88409d60095889a6a45af17c89df92b5aa712d
philippe.bernet <philippe.bernet@epita.fr>
2021-11-30 15:51:08 +0100

    fix(log): appdata instead of .exe dir

--- Holovibes/includes/holovibes_config.hh
+++ Holovibes/includes/holovibes_config.hh
@@ -12,6 +12,8 @@ namespace holovibes::settings
 #define __COMPUTE_CONFIG_FILENAME__ "compute_settings.json"
 #define __GUI_CONFIG_FILENAME__ "user_settings.ini"
 #define __CAMERAS_CONFIG_FOLDER__ "cameras_config"
+#define __EVERYTHING_LOG__ "everything.log"
+#define __LASTEST_READABLE_LOG__ "latest_readable.log"
 
 #define __APPDATA_HOLOVIBES_FOLDER_ (std::filesystem::path(getenv("AppData")) / __APPNAME__)
 #define __CONFIG_FOLDER__ (__APPDATA_HOLOVIBES_FOLDER_ / __HOLOVIBES_VERSION__)
@@ -19,5 +21,7 @@ namespace holovibes::settings
 const static std::string default_compute_config_filepath = (__CONFIG_FOLDER__ / __COMPUTE_CONFIG_FILENAME__).string();
 const static std::string global_config_filepath = (__CONFIG_FOLDER__ / __GUI_CONFIG_FILENAME__).string();
 const static std::string camera_config_folderpath = (__CONFIG_FOLDER__ / __CAMERAS_CONFIG_FOLDER__).string();
+const static std::string everything_log_path = (__CONFIG_FOLDER__ / __EVERYTHING_LOG__).string();
+const static std::string latest_readable_path = (__CONFIG_FOLDER__ / __LASTEST_READABLE_LOG__).string();
 
 } // namespace holovibes::settings
