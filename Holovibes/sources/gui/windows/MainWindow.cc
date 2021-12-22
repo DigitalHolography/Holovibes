bededf60420e99906ce0fd293f07870d77f90a18
Guillaume Poisson <guillaume.poisson@epita.fr>
2021-12-22 20:25:38 +0100

    fix(icon): Fixed icons not being shown after the installer

--- Holovibes/sources/gui/windows/MainWindow.cc
+++ Holovibes/sources/gui/windows/MainWindow.cc
@@ -79,7 +79,7 @@ MainWindow::MainWindow(QWidget* parent)
             this,
             SLOT(synchronize_thread(std::function<void()>)));
 
-    setWindowIcon(QIcon(":/Holovibes.ico"));
+    setWindowIcon(QIcon(":/holovibes_logo.png"));
 
     ::holovibes::worker::InformationWorker::display_info_text_function_ = [=](const std::string& text)
     { synchronize_thread([=]() { ui_->InfoPanel->set_text(text.c_str()); }); };
