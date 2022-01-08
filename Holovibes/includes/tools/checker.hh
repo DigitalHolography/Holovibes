0ffe69c0e38607375bfbf3e86e3387a56454d1a4
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-01-08 12:13:15 +0100

    wip(loguru): removed checker.hh

--- Holovibes/includes/tools/checker.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define LOGURU_WITH_STREAMS 1
-#include "loguru.hpp"
-
-#define CHECK(cond) DCHECK_S(cond)
--- Holovibes/includes/tools/tools.hh
+++ Holovibes/includes/tools/tools.hh
@@ -28,7 +28,7 @@ using ulong = unsigned long;
 #include "hardware_limits.hh"
 #include "frame_desc.hh"
 #include "cufft.h"
-#include "checker.hh"
+
 #include "logger.hh"
 
 #include <nlohmann/json.hpp>
