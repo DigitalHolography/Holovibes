8c4852c8ba2ad7d71fa4192c52d176fa6dbcc7a9
Damien DIDIER <damien.didier@epita.fr>
2022-09-28 17:51:05 +0200

    feat(camera-opencv): get the pixel depth from the camera, convert to grayscale and get previously set params

--- Camera/CameraOpenCV/camera_opencv.cc
+++ Camera/CameraOpenCV/camera_opencv.cc
@@ -4,6 +4,7 @@
 
 #include "opencv2/core.hpp"
 #include "opencv2/videoio.hpp"
+#include "opencv2/imgproc.hpp"
 
 namespace camera
 {
@@ -26,11 +27,7 @@ CameraOpenCV::CameraOpenCV()
     init_camera();
 }
 
-void CameraOpenCV::load_default_params()
-{
-    fd_.depth = 2;
-    fd_.byteEndian = Endianness::LittleEndian;
-}
+void CameraOpenCV::load_default_params() { fd_.byteEndian = Endianness::LittleEndian; }
 
 void CameraOpenCV::load_ini_params()
 {
@@ -39,6 +36,7 @@ void CameraOpenCV::load_ini_params()
     fd_.width = pt.get<unsigned short>("opencv.width", fd_.width);
     fd_.height = pt.get<unsigned short>("opencv.height", fd_.height);
     fps_ = pt.get<unsigned int>("opencv.fps", fps_);
+    pixel_size_ = pt.get<unsigned int>("opencv.pixel_size", pixel_size_);
 }
 
 void CameraOpenCV::bind_params()
@@ -46,7 +44,31 @@ void CameraOpenCV::bind_params()
     capture_device_.set(cv::CAP_PROP_FPS, fps_);
     capture_device_.set(cv::CAP_PROP_FRAME_WIDTH, fd_.width);
     capture_device_.set(cv::CAP_PROP_FRAME_HEIGHT, fd_.height);
-    capture_device_.set(cv::CAP_PROP_FORMAT, CV_16UC1);
+
+    int format = capture_device_.get(cv::CAP_PROP_FORMAT);
+    if (format == -1)
+    {
+        capture_device_.read(frame_);
+        format = frame_.depth();
+    }
+    fd_.depth = ((0x8442211 >> ((format & CV_MAT_DEPTH_MASK) * 4)) & 15);
+
+    int tmp_fps, tmp_width, tmp_height;
+    if ((tmp_fps = capture_device_.get(cv::CAP_PROP_FPS)) != fps_)
+    {
+        // FIXME LOG:
+        fps_ = tmp_fps;
+    }
+    if ((tmp_width = capture_device_.get(cv::CAP_PROP_FRAME_WIDTH)) != fd_.width)
+    {
+        // FIXME: LOG
+        fd_.width = tmp_width;
+    }
+    if ((tmp_height = capture_device_.get(cv::CAP_PROP_FRAME_HEIGHT)) != fd_.height)
+    {
+        // FIXME: LOG
+        fd_.height = tmp_height;
+    }
 }
 
 void CameraOpenCV::init_camera()
@@ -71,6 +93,7 @@ void CameraOpenCV::shutdown_camera() { capture_device_.release(); }
 CapturedFramesDescriptor CameraOpenCV::get_frames()
 {
     capture_device_.read(frame_);
+    cv::cvtColor(frame_, frame_, cv::COLOR_BGR2GRAY);
     return CapturedFramesDescriptor(frame_.data);
 }
 ICamera* new_camera_device() { return new CameraOpenCV(); }
