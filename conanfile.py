99c6fcd6802028efbb59db1136973bbc86cfc550
Julien Nicolle <julien.nicolle@epita.fr>
2022-10-26 11:51:47 +0200

    bump(version): 11.0.1 -> 11.1.0

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ from build import build_utils
 
 class HolovibesConan(ConanFile):
     name = "Holovibes"
-    version = "11.0.1"
+    version = "11.1.0"
     license = "GPL3"
     author = "Read AUTHORS.md"
     url = "https://holovibes.com/"
