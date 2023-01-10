50126b96e92c9243e52452c44dd801c0b2b4d712
Julien NICOLLE <julien.nicolle@epita.fr>
2023-01-10 11:19:32 +0100

    feat(s710 euresys): half of the image is working at expected frame rate

--- Camera/CameraPhantom/camera_phantom.hh
+++ Camera/CameraPhantom/camera_phantom.hh
@@ -38,18 +38,18 @@ class EHoloSubGrabber : public EGrabberCallbackOnDemand
     /*! \brief Raw pointer to the last frame captured by onNewBufferEvent. */
     uint8_t* last_ptr_;
 
-  private:
-    virtual void onNewBufferEvent(const NewBufferData& data)
-    {
-        /* Using ScopedBuffer will tell the grabber that the buffer is available
-         * to store new acquired frames at the end of this scope. This behavior
-         * is not an issue as the next time onNewBufferEvent will be called, the
-         * previous frame will have already been enqueued in Holovibes input
-         * queue.
-         */
-        ScopedBuffer buffer(*this, data);
-        last_ptr_ = static_cast<uint8_t*>(buffer.getUserPointer());
-    }
+  //private:
+    //virtual void onNewBufferEvent(const NewBufferData& data)
+    //{
+    //    /* Using ScopedBuffer will tell the grabber that the buffer is available
+    //     * to store new acquired frames at the end of this scope. This behavior
+    //     * is not an issue as the next time onNewBufferEvent will be called, the
+    //     * previous frame will have already been enqueued in Holovibes input
+    //     * queue.
+    //     */
+    //    ScopedBuffer buffer(*this, data);
+    //    last_ptr_ = static_cast<uint8_t*>(buffer.getUserPointer());
+    //}
 };
 
 /*! \class EHoloGrabber
@@ -71,18 +71,22 @@ class EHoloSubGrabber : public EGrabberCallbackOnDemand
 class EHoloGrabber
 {
   public:
-    EHoloGrabber(EGenTL& gentl)
+    EHoloGrabber(EGenTL& gentl, unsigned int nb_images_per_buffer)
         : grabbers_(gentl)
+        , nb_images_per_buffer_(nb_images_per_buffer)
     {
         // Fetch the first grabber info to determine the width, height and depth
         // of the full image.
         // According to the requirements described above, we assume that the
         // full height is two times the height of the first grabber.
 
+        grabbers_[0]->setInteger<StreamModule>("BufferPartCount", 1);
         width_ = grabbers_[0]->getWidth();
         height_ = grabbers_[0]->getHeight() * grabbers_.length();
-        std::string pixel_format = grabbers_[0]->getPixelFormat();
-        depth_ = gentl.imageGetBytesPerPixel(pixel_format);
+        depth_ = gentl.imageGetBytesPerPixel(grabbers_[0]->getPixelFormat());
+
+        for (unsigned i = 0; i < grabbers_.length(); ++i)
+            grabbers_[i]->setInteger<StreamModule>("BufferPartCount", nb_images_per_buffer_);
     }
 
     virtual ~EHoloGrabber()
@@ -90,10 +94,7 @@ class EHoloGrabber
         for (size_t i = 0; i < grabbers_.length(); i++)
             grabbers_[i]->reallocBuffers(0);
 
-        for (size_t i = 0; i < buffers_.size(); i++)
-            cudaFreeHost(buffers_[i]);
-
-        buffers_.clear();
+        cudaFreeHost(ptr_);
     }
 
     void init(unsigned int nb_buffers)
@@ -106,22 +107,23 @@ class EHoloGrabber
         // Learn more about pinned memory:
         // https://developer.nvidia.com/blog/how-optimize-data-transfers-cuda-cc/.
 
-        buffers_.reserve(nb_buffers);
-        while (buffers_.size() < nb_buffers)
+        uint8_t *device_ptr;
+
+        cudaError_t alloc_res = cudaHostAlloc(&ptr_, frame_size * nb_images_per_buffer_ * nb_buffers_, cudaHostAllocMapped);
+        cudaError_t device_ptr_res = cudaHostGetDevicePointer(&device_ptr, ptr_, 0);
+
+        if (alloc_res != cudaSuccess || device_ptr_res != cudaSuccess)
+            Logger::camera()->error("Could not allocate buffers.");
+
+        for (size_t i = 0; i < nb_buffers; ++i)
         {
             // The EGrabber API can handle directly buffers alocated in pinned
             // memory as we just have to use cudaHostAlloc and give each grabber
             // the host pointer and the associated pointer in device memory.
-            uint8_t *ptr, *device_ptr;
-            cudaError_t alloc_res = cudaHostAlloc(&ptr, frame_size, cudaHostAllocMapped);
-            cudaError_t device_ptr_res = cudaHostGetDevicePointer(&device_ptr, ptr, 0);
 
-            if (alloc_res != cudaSuccess || device_ptr_res != cudaSuccess)
-                Logger::camera()->error("Could not allocate buffers.");
-
-            buffers_.push_back(ptr);
+            size_t offset = i * frame_size * nb_images_per_buffer_;
             for (size_t ix = 0; ix < grabber_count; ix++)
-                grabbers_[ix]->announceAndQueue(UserMemory(ptr, frame_size, device_ptr));
+                grabbers_[ix]->announceAndQueue(UserMemory(ptr_ + offset, frame_size * nb_images_per_buffer_, device_ptr + offset));
         }
     }
 
@@ -131,23 +133,20 @@ class EHoloGrabber
 
         // Start each sub grabber in reverse order
         for (size_t i = 0; i < grabber_count; i++)
-        {
-            grabbers_[grabber_count - 1 - i]->enableEvent<NewBufferData>();
             grabbers_[grabber_count - 1 - i]->start();
-        }
     }
 
-    void* get_frame()
-    {
-        // For each grabber, if a new frame has been written into memory within
-        // FRAME_TIMEOUT ms we call onNewBufferEvent. Otherwise, a timeout
-        // exception will be thrown. This part of the code is thus blocking!
-        for (size_t i = 0; i < grabbers_.length(); i++)
-            grabbers_[i]->processEvent<NewBufferData>(FRAME_TIMEOUT);
+    //void* get_frame()
+    //{
+    //    // For each grabber, if a new frame has been written into memory within
+    //    // FRAME_TIMEOUT ms we call onNewBufferEvent. Otherwise, a timeout
+    //    // exception will be thrown. This part of the code is thus blocking!
+    //    for (size_t i = 0; i < grabbers_.length(); i++)
+    //        grabbers_[i]->processEvent<NewBufferData>(FRAME_TIMEOUT);
 
-        // The first and second grabber last_ptr_ is the identical.
-        return grabbers_[0]->last_ptr_;
-    }
+    //    // The first and second grabber last_ptr_ is the identical.
+    //    return grabbers_[0]->last_ptr_;
+    //}
 
     void stop()
     {
@@ -164,20 +163,26 @@ class EHoloGrabber
     /*! \brief The depth of the acquired frames. */
     unsigned int depth_;
 
+    /*! \brief An EGrabbers instance composed of the two EHoloSubGrabber grabbers.  */
+    EGrabbers<EHoloSubGrabber> grabbers_;
+
   private:
     /*! \brief Unique ptr to the instance of the GenICam GenTL API. */
     std::unique_ptr<EGenTL> gentl_;
 
-    /*! \brief An EGrabbers instance composed of the two EHoloSubGrabber grabbers.  */
-    EGrabbers<EHoloSubGrabber> grabbers_;
 
     /*! \brief The number of buffers used to store frames. It is equivalent to
      * the number of frames to store simultaneously.
      */
     unsigned int nb_buffers_;
 
-    /*! \brief A vector storing all host memory pointers allocated to later free them. */
-    std::vector<uint8_t*> buffers_;
+    /*! \brief The number of images stored in each buffers.
+     */
+    unsigned int nb_images_per_buffer_;
+
+    /*! \brief A pointer the cuda memory allocated for the buffers.
+    */
+    uint8_t* ptr_;
 };
 
 class CameraPhantom : public Camera
@@ -201,5 +206,6 @@ class CameraPhantom : public Camera
     std::unique_ptr<EHoloGrabber> grabber_;
 
     unsigned int nb_buffers_;
+    unsigned int nb_images_per_buffer_;
 };
 } // namespace camera
