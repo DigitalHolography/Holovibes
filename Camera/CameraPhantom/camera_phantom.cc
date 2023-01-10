1f134db8a5999cfe7cee7968db40d745ff44110a
Julien NICOLLE <julien.nicolle@epita.fr>
2023-01-10 11:19:32 +0100

    feat(s710 euresys): half of the image is working at expected frame rate

--- Camera/CameraPhantom/camera_phantom.cc
+++ Camera/CameraPhantom/camera_phantom.cc
@@ -13,9 +13,6 @@ namespace camera
 CameraPhantom::CameraPhantom()
     : Camera("phantom.ini")
 {
-    gentl_ = std::make_unique<Euresys::EGenTL>();
-    grabber_ = std::make_unique<EHoloGrabber>(*gentl_);
-
     name_ = "Phantom S710";
     pixel_size_ = 20;
 
@@ -25,6 +22,9 @@ CameraPhantom::CameraPhantom()
         load_ini_params();
         ini_file_.close();
     }
+
+    gentl_ = std::make_unique<Euresys::EGenTL>();
+    grabber_ = std::make_unique<EHoloGrabber>(*gentl_, nb_images_per_buffer_);
 
     init_camera();
 }
@@ -48,15 +48,35 @@ void CameraPhantom::shutdown_camera() { return; }
 
 CapturedFramesDescriptor CameraPhantom::get_frames()
 {
-    return CapturedFramesDescriptor(grabber_->get_frame(), 1, true);
+    ScopedBuffer buffer(*(grabber_->grabbers_[0]));
+    uint8_t *bufferPtr = buffer.getInfo<uint8_t *>(gc::BUFFER_INFO_BASE);
+    // size_t imageSize = buffer.getInfo<size_t>(ge::BUFFER_INFO_CUSTOM_PART_SIZE);
+
+    // process available images
+    size_t delivered = buffer.getInfo<size_t>(ge::BUFFER_INFO_CUSTOM_NUM_DELIVERED_PARTS);
+
+    CapturedFramesDescriptor ret;
+
+    ret.on_gpu = false;
+    ret.region1 = buffer.getUserPointer();
+    ret.count1 = delivered;
+    ret.count2 = 0;
+
+    return ret;
 }
 
-void CameraPhantom::load_default_params() { nb_buffers_ = 64; }
+
+void CameraPhantom::load_default_params()
+{
+    nb_buffers_ = 16;
+    nb_images_per_buffer_ = 16;
+}
 
 void CameraPhantom::load_ini_params()
 {
     const boost::property_tree::ptree& pt = get_ini_pt();
     nb_buffers_ = pt.get<unsigned int>("phantom.nb_buffers", nb_buffers_);
+    nb_images_per_buffer_ = pt.get<unsigned int>("phantom.nb_images_per_buffer", nb_images_per_buffer_);
 }
 
 void CameraPhantom::bind_params() { return; }
