cf6f271c3fe968a1ddcafa96c436cf737d69ce55
Yaktusk <computer.yakutsk@gmail.com>
2024-01-18 15:57:52 +0100

    bump(version): 12.5.0 -> 12.6.0

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ from build import build_utils
 
 class HolovibesConan(ConanFile):
     name = "Holovibes"
-    version = "12.5.0"
+    version = "12.6.0"
     license = "GPL3"
     author = "Read AUTHORS.md"
     url = "https://holovibes.com/"
