9ee3f2b399afa9fcac8665f3d99bfc4a19c65d32
MaxBA1602 <maxba1602@gmail.com>
2024-05-22 13:50:19 +0200

    Slot and ui correctly binded

--- Holovibes/includes/gui/windows/lightui.hh
+++ Holovibes/includes/gui/windows/lightui.hh
@@ -21,6 +21,25 @@ public:
     explicit LightUI(QWidget *parent = nullptr, MainWindow* main_window = nullptr);
     ~LightUI();
 
+public slots:
+    /*! \brief Opens file explorer on the fly to let the user chose the output file he wants with extension
+     * replacement*/
+    void browse_record_output_file();
+
+    /**
+     * @brief Handles the update of the record file path setting line edit.
+     */
+    void update_record_file_path();
+
+    /*! \brief Enables or Disables number of frame restriction for recording
+     *
+     * \param value true: enable, false: disable
+     */
+    void set_nb_frames_mode(bool value);
+
+    /*! \brief Start/Stops the record */
+    void start_stop_recording(bool start);
+
 private:
     Ui::LightUI *ui_;
     MainWindow* main_window_;
--- Holovibes/sources/gui/windows/lightui.cc
+++ Holovibes/sources/gui/windows/lightui.cc
@@ -1,4 +1,5 @@
 #include "lightui.hh"
+// #include "export_panel.hh"
 #include "MainWindow.hh"
 #pragma warning(push, 0)
 #include "ui_lightui.h"
@@ -11,10 +12,47 @@ LightUI::LightUI(QWidget *parent, MainWindow* main_window)
     , ui_(new Ui::LightUI), main_window_(main_window)
 {
     ui_->setupUi(this);
+
+    /*
+    QHBoxLayout *outputFileSelectionLayout;
+    holovibes::gui::Drag_drop_lineedit *OutputFilePathLineEdit;
+    QToolButton *OutputFileBrowseToolButton;
+    QPushButton *startButton;
+    */
+    connect(ui_->OutputFileBrowseToolButton, &QPushButton::clicked, this, &LightUI::browse_record_output_file);
+    connect(ui_->OutputFilePathLineEdit, &QLineEdit::textChanged, this, &LightUI::update_record_file_path);
+    connect(ui_->startButton, &QPushButton::toggled, this, &LightUI::start_stop_recording);
 }
 
 LightUI::~LightUI()
 {
     delete ui_;
 }
+
+void LightUI::browse_record_output_file() {
+    LOG_INFO("Browsing record output file");
+}
+
+void LightUI::update_record_file_path() {
+    LOG_INFO("Updating record file path");
+}
+
+void LightUI::start_stop_recording(bool start) {
+    char str[20];
+    if (start)
+        strcpy(str, "Start recording");
+    else
+        strcpy(str, "Stop recording");
+    LOG_INFO(str);
+}
+
+void LightUI::set_nb_frames_mode(bool value) {
+    char str[50];
+    if (value)
+        strcpy(str, "Enable number of frame restriction");
+    else
+        strcpy(str, "Disable number of frame restriction");
+    LOG_INFO(str);
+}
+
 }
