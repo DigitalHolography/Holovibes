0fd7c0e8d2ee5064752c3f7f62b947b5ca7e8bbc
NoeTopeza <noe.topeza@epita.fr>
2024-07-08 13:35:18 +0200

    added more documentation

--- Holovibes/includes/gui/selections/interfaces/rect_overlay.hh
+++ Holovibes/includes/gui/selections/interfaces/rect_overlay.hh
@@ -1,6 +1,6 @@
 /*! \file
  *
- * \brief Interface for all rectangular overlays.
+ * \brief Interface for all rectangular overlays in the Holovibes GUI.
  */
 #pragma once
 
@@ -10,22 +10,41 @@ namespace holovibes::gui
 {
 /*! \class RectOverlay
  *
- * \brief #TODO Add a description for this class
+ * \brief Manages rectangular-shaped overlays.
+ *
+ * This class provides functionality for overlays that maintain a rectangular shape,
+ * extending the capabilities of the base Overlay class.
  */
 class RectOverlay : public Overlay
 {
   public:
+    /*! \brief Constructor
+     *
+     * \param overlay The kind of overlay.
+     * \param parent Pointer to the parent BasicOpenGLWindow.
+     */
     RectOverlay(KindOfOverlay overlay, BasicOpenGLWindow* parent);
 
+    /*! \brief Destructor */
     virtual ~RectOverlay() {}
 
+    /*! \brief Initializes the rectangular overlay. */
     virtual void init() override;
+
+    /*! \brief Draws the rectangular overlay using OpenGL functions. */
     virtual void draw() override;
 
+    /*! \brief Handles mouse move events.
+     *
+     * \param e Pointer to the QMouseEvent.
+     */
     virtual void move(QMouseEvent* e) override;
+
+    /*! \brief Checks if the corners of the rectangle are within bounds. */
     virtual void checkCorners();
 
   protected:
+    /*! \brief Sets the vertex buffer with the coordinates of the rectangle. */
     void setBuffer() override;
 };
 } // namespace holovibes::gui
--- Holovibes/includes/gui/selections/interfaces/square_overlay.hh
+++ Holovibes/includes/gui/selections/interfaces/square_overlay.hh
@@ -1,6 +1,6 @@
 /*! \file
  *
- * \brief Interface for all square overlays.
+ * \brief Interface for all square overlays in the Holovibes GUI.
  */
 #pragma once
 
@@ -10,20 +10,34 @@ namespace holovibes::gui
 {
 /*! \class SquareOverlay
  *
- * \brief #TODO Add a description for this class
+ * \brief Manages square-shaped overlays.
+ *
+ * This class provides functionality specific to overlays that maintain a square shape,
+ * extending the capabilities of RectOverlay.
  */
 class SquareOverlay : public RectOverlay
 {
   public:
+    /*! \brief Constructor
+     *
+     * \param overlay The kind of overlay.
+     * \param parent Pointer to the parent BasicOpenGLWindow.
+     */
     SquareOverlay(KindOfOverlay overlay, BasicOpenGLWindow* parent);
 
+    /*! \brief Destructor */
     virtual ~SquareOverlay() {}
 
-    /*! \brief Check if corners don't go out of bounds. */
+    /*! \brief Checks if the corners of the square are within bounds. */
     void checkCorners() override;
-    /*! \brief Change the rectangular zone to a square zone, using the shortest side */
+
+    /*! \brief Converts the rectangular zone to a square zone, using the shortest side. */
     void make_square();
 
+    /*! \brief Handles mouse move events.
+     *
+     * \param e Pointer to the QMouseEvent.
+     */
     virtual void move(QMouseEvent* e) override;
 };
 } // namespace holovibes::gui
