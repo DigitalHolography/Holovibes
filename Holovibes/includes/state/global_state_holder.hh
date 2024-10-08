a460c604dc8ae9950ad2175548d53cf7140d505d
Hpn4 <etienne.senigout@epita.fr>
2024-10-08 14:17:59 +0200

    refacto: delete map_macro.hh and map_macro_msvc.hh

--- Holovibes/includes/state/global_state_holder.hh
+++ Holovibes/includes/state/global_state_holder.hh
@@ -18,24 +18,6 @@ namespace holovibes
  *
  * \brief The GSH (global state holder), is where all the global state of the program is stored.
  *
- * Its goal is to register changes commanded by the API (which comes from user events), dispatch the changes
- * to each worker, and provide informations queried by the API. It relies on several structures and mecanisms :
- *
- * Queries and Commands : every data needed by the API shall be obtained in the form of structured Queries, provided by
- * the GSH. This guarantees that only needed data is accessed to, and grants better code readability.
- * The same principle is applied to changes comming from the API, which come in the form of structured Commands.
- *
- * MicroCache : local state holder belonging to a worker. Previously, each worker had to fetch data at the same
- * place ; therefore, all variables had to be atomic, with the aim to be thread safe. Furthermore, since global state
- * was used in the pipe, directly modifying the state was often not possible (changing operations or variables which
- * have impact on buffers'size would have caused incoherent computations and/or segfaults and undefined behaviors). The
- * ComputeWorker now possess MicroCaches, containing all the state it needs. Those MicroCaches are accessed only by
- * their worker, and are synchronized with the GSH when each worker chooses to (using a trigger system). The
- * implementation of MicroCaches enabled the direct modification of the state, since the state used in the pipe is now
- * desynchronized from the GSH.
- * More informations and explanations concerning their synchronization with the GSH are provided in files micro-cache.hh
- * and micro-cache.hxx.
- *
  * FastUpdateHolder : the fastUpdateHolder is a templated map which is used by the informationWorker to access and
  * display information (like fps and queue occupancy) at a high rate, since this needs to be updated continuously.
  */
