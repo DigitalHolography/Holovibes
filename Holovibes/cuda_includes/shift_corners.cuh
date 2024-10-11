aeae238e03f3b0631ca993feda2d15acf86f86f3
Marius Dubosc <mariusdubosc@gmail.com>
2024-10-11 11:55:26 +0200

    fix: rename shift corners

--- Holovibes/cuda_includes/shift_corners.cuh
+++ Holovibes/cuda_includes/shift_corners.cuh
@@ -37,29 +37,29 @@ void shift_corners(
  * of the spectrum (and vice-versa), as explained in the matlab documentation
  * (http://fr.mathworks.com/help/matlab/ref/fftshift.html).
  *
- * \param input The image to shift.
  * \param output The destination image
+ * \param input The image to shift.
  * \param batch_size Number of images in input
  * \param size_x The width of data, in pixels.
  * \param size_y The height of data, in pixels.
  * \param stream The CUDA stream on which to launch the operation.
  */
-void shift_corners(const float3* input,
-                   float3* output,
+void shift_corners(float3* output,
+                   const float3* input,
                    const uint batch_size,
                    const uint size_x,
                    const uint size_y,
                    const cudaStream_t stream);
 
-void shift_corners(const float* input,
-                   float* output,
+void shift_corners(float* output,
+                   const float* input,
                    const uint batch_size,
                    const uint size_x,
                    const uint size_y,
                    const cudaStream_t stream);
 
-void shift_corners(const cuComplex* input,
-                   cuComplex* output,
+void shift_corners(cuComplex* output,
+                   const cuComplex* input,
                    const uint batch_size,
                    const uint size_x,
                    const uint size_y,
