5bd1f6ba7a2010ba88e968d6f9a793655d2cacd5
Simon Riou <simonriou3492@gmail.com>
2024-10-17 11:13:23 +0200

    fix: precommit jsp quoi

--- Holovibes/sources/thread/camera_frame_read_worker.cc
+++ Holovibes/sources/thread/camera_frame_read_worker.cc
@@ -79,7 +79,8 @@ void CameraFrameReadWorker::enqueue_loop(const camera::CapturedFramesDescriptor&
 
     processed_frames_ += captured_fd.count1 + captured_fd.count2;
     compute_fps();
-    *temperature_ = std::to_string(camera_->get_temperature());
+    *temperature_ =
+        camera_->get_temperature() == 0 ? "No temperature available" : std::to_string(camera_->get_temperature());
 
     if (input_queue_on_gpu)
         input_queue_.load()->sync_current_batch();
