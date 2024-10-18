038bcc0ed2b80304e53caf1ad70ad0fee3c906a1
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2024-10-18 11:16:25 +0200

    fix: fix nb_grabbers_ to nb_grabbers

--- Camera/CamerasPhantom/AmetekS991EuresysCoaxlinkQsfp+/camera_phantom_s991.cc
+++ Camera/CamerasPhantom/AmetekS991EuresysCoaxlinkQsfp+/camera_phantom_s991.cc
@@ -11,7 +11,7 @@ EHoloGrabber::EHoloGrabber(Euresys::EGenTL& gentl,
                            unsigned int buffer_part_count,
                            std::string pixel_format,
                            unsigned int nb_grabbers)
-    : EHoloGrabberInt(gentl, buffer_part_count, pixel_format, nb_grabbers_)
+    : EHoloGrabberInt(gentl, buffer_part_count, pixel_format, nb_grabbers)
 {
     if (available_grabbers_.size() < nb_grabbers_)
     {
