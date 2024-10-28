822fd6589b38bd07a989d7785562a7d3c29517ef
Hpn4 <etienne.senigout@epita.fr>
2024-10-28 10:36:06 +0100

    fix: no more crash because of qwidget

--- Holovibes/sources/gui/other/popup_error.cc
+++ Holovibes/sources/gui/other/popup_error.cc
@@ -5,15 +5,30 @@ namespace holovibes::gui
 {
 void show_error_and_exit(const std::string& error_msg, const int exit_value)
 {
-    QMessageBox messageBox;
-    messageBox.critical(nullptr, "Internal Error", error_msg.c_str());
-    exit(exit_value);
+    if (!holovibes::Holovibes::instance().is_cli)
+    {
+        QMessageBox messageBox;
+        messageBox.critical(nullptr, "Internal Error", error_msg.c_str());
+        exit(exit_value);
+    }
+    else
+    {
+        LOG_ERROR(error_msg);
+        exit(exit_value);
+    }
 }
 
 void show_warn(const std::string& warn_msg)
 {
-    QMessageBox messageBox;
-    messageBox.warning(nullptr, "Warning!", warn_msg.c_str());
+    if (!holovibes::Holovibes::instance().is_cli)
+    {
+        QMessageBox messageBox;
+        messageBox.warning(nullptr, "Warning!", warn_msg.c_str());
+    }
+    else
+    {
+        LOG_WARN(warn_msg);
+    }
 }
 
 } // namespace holovibes::gui
