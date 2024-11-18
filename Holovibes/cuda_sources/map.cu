2c1c5a2bed3568a8146d76d9d11aa85d10cf64b0
Arthur Courselle <courselle.arthur@gmail.com>
2024-11-18 16:14:34 +0100

    refacto: Remove map.cuhxx and adapted the cuh to keep templated functions.

--- Holovibes/cuda_sources/map.cu
+++ Holovibes/cuda_sources/map.cu
@@ -4,7 +4,6 @@
 #include "common.cuh"
 #include "reduce.cuh"
 
-/***** Overloaded and specific map implementations *****/
 void map_log10(float* const output, const float* const input, const size_t size, const cudaStream_t stream)
 {
     static const auto log10 = [] __device__(const float input_pixel) { return log10f(input_pixel); };
@@ -12,19 +11,18 @@ void map_log10(float* const output, const float* const input, const size_t size,
     map_generic(output, input, size, log10, stream);
 }
 
-// It is mandatory to declare and implement these functions
-// with float array parameters in order to be called from .cc
-
 void map_divide(
     float* const output, const float* const input, const size_t size, const float value, const cudaStream_t stream)
 {
-    // Call templated version map divide
-    map_divide<float>(output, input, size, value, stream);
+    const auto divide = [value] __device__(const float input_pixel) -> float { return input_pixel / value; };
+
+    map_generic(output, input, size, divide, stream);
 }
 
 void map_multiply(
     float* const output, const float* const input, const size_t size, const float value, const cudaStream_t stream)
 {
-    // Call templated version map multiply
-    map_multiply<float>(output, input, size, value, stream);
+    const auto multiply = [value] __device__(const float input_pixel) -> float { return input_pixel * value; };
+
+    map_generic(output, input, size, multiply, stream);
 }
