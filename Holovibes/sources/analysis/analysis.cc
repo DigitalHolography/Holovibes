6db92ae7e84846290fd0a727d7ba6a2f1ebd5cf5
ArthurCourselle <courselle.arthur@gmail.com>
2024-11-25 09:58:33 +0100

    fix: fix missing not operator for err check of resize

--- Holovibes/sources/analysis/analysis.cc
+++ Holovibes/sources/analysis/analysis.cc
@@ -79,21 +79,21 @@ void Analysis::init()
     vesselness_mask_env_.time_window_ = api::get_time_window();
     vesselness_mask_env_.number_image_mean_ = 0;
 
-    err += vesselness_mask_env_.m0_ff_sum_image_.resize(buffers_.gpu_postprocess_frame_size);
-    err += vesselness_mask_env_.image_with_mean_.resize(buffers_.gpu_postprocess_frame_size);
-    err += vesselness_mask_env_.m0_ff_video_centered_.resize(buffers_.gpu_postprocess_frame_size *
-                                                             506); // TODO: time_window
+    err += !vesselness_mask_env_.m0_ff_sum_image_.resize(buffers_.gpu_postprocess_frame_size);
+    err += !vesselness_mask_env_.image_with_mean_.resize(buffers_.gpu_postprocess_frame_size);
+    err += !vesselness_mask_env_.m0_ff_video_centered_.resize(buffers_.gpu_postprocess_frame_size *
+                                                              506); // TODO: time_window
 
-    err += vesselness_mask_env_.vascular_image_.resize(frame_res);
+    err += !vesselness_mask_env_.vascular_image_.resize(frame_res);
 
     // Allocate vesselness filter struct internal buffers
-    err += vesselness_filter_struct_.I.resize(frame_res);
-    err += vesselness_filter_struct_.convolution_tmp_buffer.resize(frame_res);
-    err += vesselness_filter_struct_.H.resize(frame_res * 3);
-    err += vesselness_filter_struct_.lambda_1.resize(frame_res);
-    err += vesselness_filter_struct_.lambda_2.resize(frame_res);
-    err += vesselness_filter_struct_.R_blob.resize(frame_res);
-    err += vesselness_filter_struct_.c_temp.resize(frame_res);
+    err += !vesselness_filter_struct_.I.resize(frame_res);
+    err += !vesselness_filter_struct_.convolution_tmp_buffer.resize(frame_res);
+    err += !vesselness_filter_struct_.H.resize(frame_res * 3);
+    err += !vesselness_filter_struct_.lambda_1.resize(frame_res);
+    err += !vesselness_filter_struct_.lambda_2.resize(frame_res);
+    err += !vesselness_filter_struct_.R_blob.resize(frame_res);
+    err += !vesselness_filter_struct_.c_temp.resize(frame_res);
 
     // Compute gaussian deriviatives kernels according to simga
     float sigma = setting<settings::VesselnessSigma>();
@@ -128,7 +128,7 @@ void Analysis::init()
 
     cudaXStreamSynchronize(stream_);
 
-    err += vesselness_mask_env_.g_xx_mul_.resize(x_size * y_size);
+    err += !vesselness_mask_env_.g_xx_mul_.resize(x_size * y_size);
     holovibes::compute::matrix_multiply<float>(g_xx_qy,
                                                g_xx_px,
                                                y_size,
@@ -168,7 +168,7 @@ void Analysis::init()
     cudaXMalloc(&g_xy_qy, y_size * sizeof(float));
     comp_dgaussian(g_xy_qy, y, y_size, sigma, 1, stream_);
 
-    err += vesselness_mask_env_.g_xy_mul_.resize(x_size * y_size);
+    err += !vesselness_mask_env_.g_xy_mul_.resize(x_size * y_size);
     holovibes::compute::matrix_multiply<float>(g_xy_qy,
                                                g_xy_px,
                                                y_size,
@@ -182,13 +182,13 @@ void Analysis::init()
                                CUBLAS_OP_T,
                                CUBLAS_OP_N,
                                x_size,
-                               y_size, // Dimensions de B
+                               y_size,
                                &alpha,
                                vesselness_mask_env_.g_xy_mul_,
-                               y_size, // Source (A), pas de ligne de A
-                               &beta,
+                               y_size,
+                               A & beta,
                                nullptr,
-                               y_size, // B est nul, donc on utilise 0
+                               y_size,
                                result_transpose,
                                x_size));
 
@@ -206,7 +206,7 @@ void Analysis::init()
     comp_dgaussian(g_yy_qy, x, y_size, sigma, 2, stream_);
 
     // Compute qy * px matrices to simply two 1D convolutions to one 2D convolution
-    err += vesselness_mask_env_.g_yy_mul_.resize(x_size * y_size);
+    err += !vesselness_mask_env_.g_yy_mul_.resize(x_size * y_size);
     holovibes::compute::matrix_multiply<float>(g_yy_qy,
                                                g_yy_px,
                                                y_size,
@@ -220,13 +220,13 @@ void Analysis::init()
                                CUBLAS_OP_T,
                                CUBLAS_OP_N,
                                x_size,
-                               y_size, // Dimensions de B
+                               y_size,
                                &alpha,
                                vesselness_mask_env_.g_yy_mul_,
-                               y_size, // Source (A), pas de ligne de A
+                               y_size,
                                &beta,
                                nullptr,
-                               y_size, // B est nul, donc on utilise 0
+                               y_size,
                                result_transpose,
                                x_size));
 
@@ -239,8 +239,8 @@ void Analysis::init()
     float sigma_2 = 0.02 * fd_.width;
     vesselness_mask_env_.vascular_kernel_size_ = 2 * std::ceil(2 * sigma_2) + 1;
 
-    err += vesselness_mask_env_.vascular_kernel_.resize(vesselness_mask_env_.vascular_kernel_size_ *
-                                                        vesselness_mask_env_.vascular_kernel_size_);
+    err += !vesselness_mask_env_.vascular_kernel_.resize(vesselness_mask_env_.vascular_kernel_size_ *
+                                                         vesselness_mask_env_.vascular_kernel_size_);
     compute_gauss_kernel(vesselness_mask_env_.vascular_kernel_, sigma_2, stream_);
 
     if (err != 0)
