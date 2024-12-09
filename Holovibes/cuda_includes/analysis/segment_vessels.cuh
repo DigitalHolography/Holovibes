c81657e36bee7c90cdac08e5eacc4e5bd4c56d7b
ArthurCourselle <courselle.arthur@gmail.com>
2024-12-09 11:18:02 +0100

    refactor: add const in arguments of segment_vessels functions

--- Holovibes/cuda_includes/analysis/segment_vessels.cuh
+++ Holovibes/cuda_includes/analysis/segment_vessels.cuh
@@ -1,6 +1,6 @@
-/*! \file
+/*! \file segment_vessels.cuh
  *
- * \brief #TODO Add a description for this file
+ * \brief Contains all functions tied to the vessels segmentation for analysis
  */
 #pragma once
 
@@ -8,16 +8,22 @@
 
 typedef unsigned int uint;
 
-void segment_vessels(float* output,
-                     float* new_thresholds,
-                     float* R_VascularPulse,
-                     float* mask_vesselness_clean,
-                     uint size,
-                     float* thresholds,
-                     cudaStream_t stream);
+void segment_vessels(float* const output,
+                     float* const new_thresholds,
+                     float* const R_VascularPulse,
+                     const float* const mask_vesselness_clean,
+                     const size_t size,
+                     const float* thresholds,
+                     const cudaStream_t stream);
 
-void compute_first_mask_artery(float* output, float* input, uint size, cudaStream_t stream);
+void compute_first_mask_artery(float* const output,
+                               const float* const input,
+                               const size_t size,
+                               const cudaStream_t stream);
 
-void compute_first_mask_vein(float* output, float* input, uint size, cudaStream_t stream);
+void compute_first_mask_vein(float* const output,
+                             const float* const input,
+                             const size_t size,
+                             const cudaStream_t stream);
 
-void negation(float* input_output, uint size, cudaStream_t stream);
+void negation(float* const input_output, const size_t size, const cudaStream_t stream);
