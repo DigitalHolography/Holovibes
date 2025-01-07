e38c2fd8bf0e8a92efcc15c6cce445a6b56373fd
Arthur Courselle <courselle.arthur@gmail.com>
2025-01-07 15:19:30 +0100

    doc: adding documentation for cufft class since it has never been done.

--- Holovibes/includes/tools/cuda_tools/cufft_handle.hh
+++ Holovibes/includes/tools/cuda_tools/cufft_handle.hh
@@ -1,6 +1,6 @@
-/*! \file
+/*! \file cufft_handle.hh
  *
- * \brief Declaration of the CufftHandle class
+ *  \brief Declaration of the CufftHandle class. Used to wrap the `cufftHandle` class of CUDA.
  */
 #pragma once
 
@@ -13,7 +13,7 @@ namespace holovibes::cuda_tools
 {
 /*! \class CufftHandle
  *
- * \brief RAII wrapper for cufftHandle
+ *  \brief RAII wrapper for CUDA cufftHandle.
  */
 class CufftHandle
 {
@@ -21,9 +21,22 @@ class CufftHandle
     /*! \brief Default constructor */
     CufftHandle();
 
+    /*! \brief Setter for the current stream used for cufft API.
+     *  \param[in] stream The new stream.
+     */
     static void set_stream(const cudaStream_t& stream);
 
-    /*! \brief Constructor calling plan2d */
+    /*! \brief Constructor calling plan2d function.
+     *
+     *  Be careful, the x and y are inversed in cuda since the cufft API uses col-major.
+     *  Hence use the height for the x param and width for the y param.
+     *  For more information go check the following cufft documentation (nx and ny params):
+     *  https://docs.nvidia.com/cuda/cufft/#cufftplan2d
+     *
+     *  \param[in] x The height of the plan.
+     *  \param[in] y The width of the plan.
+     *  \param[in] type The type of the cufft plan.
+     */
     CufftHandle(const int x, const int y, const cufftType type);
 
     /*! \brief Destroy the created plan (if any) */
@@ -32,13 +45,40 @@ class CufftHandle
     /*! \brief Destroy the created plan (if any) */
     void reset();
 
-    /*! \brief Calls plan2d
+    /*! \brief Calls cufftPlan2d to get a 2d plan.
      *
-     * Could be overloaded for plan1d and plan3d
+     *  Could be overloaded for plan1d and plan3d.
+     *  Be careful, the x and y are inversed in cuda since the cufft API uses col-major.
+     *  Hence use the height for the x param and width for the y param.
+     *  For more information go check the following cufft documentation (nx and ny params):
+     *  https://docs.nvidia.com/cuda/cufft/#cufftplan2d
+     *
+     *  \param[in] x The height of the plan.
+     *  \param[in] y The width of the plan.
+     *  \param[in] type The transform data type.
      */
     void plan(const int x, const int y, const cufftType type);
 
-    /*! \brief Calls planMany */
+    /*! \brief Calls cufftPlanMany.
+     *
+     *  \param[in] rank Dimensionality of the transform.
+     *  \param[in] n Array of size `rank`, describing the size of each dimension, `n[0]` being the size of the outermost
+     *  and `n[rank-1]` innermost (contiguous) dimension of a transform.
+     *  \param[in] inembed Pointer of size `rank` that indicates the storage dimensions of the input data in memory.
+     *  If set to NULL all other advanced data layout parameters are ignored.
+     *  \param[in] istride Indicates the distance between two successive input elements in the least significant
+     *  (i.e., innermost) dimension.
+     *  \param[in] idist Indicates the distance between the first element of two consecutive signals in a batch of the
+     *  input data.
+     *  \param[in] onembed Pointer of size `rank` that indicates the storage dimensions of the output data in memory. If
+     *  set to NULL all other advanced data layout parameters are ignored.
+     *  \param[in] ostride Indicates the distance between two successive output elements in the output array in the
+     *  least significant (i.e., innermost) dimension.
+     *  \param[in] odist Indicates the distance between the first element of two consecutive signals in a batch of the
+     *  output data.
+     *  \param[in] type The transform data type.
+     *  \param[in] batch Batch size for this transform.
+     */
     void planMany(int rank,
                   int* n,
                   int* inembed,
@@ -50,15 +90,34 @@ class CufftHandle
                   cufftType type,
                   int batch);
 
-    /*! \brief Calls XtplanMany
+    /*! \brief Calls cufftXtMakePlanMany.
      *
-     * Warning ! Allocating a plan has a VRAM memory cost :
-     * You can check the amount of memory used by checking the ws (working size)
-     * variable
+     *  Warning: Allocating a plan has a VRAM memory cost.
+     *  You can check the amount of memory used by checking the ws (working size) variable.
      *
-     * The memory taken by the plan is used by cufft to make the FFT
-     * It's equal in byte to : frame_width * frame_height * sizeof(T) (in our
-     * case cuComplex) * batch_size
+     *  The memory taken by the plan is used by cufft to make the FFT.
+     *  It's equal in byte to : frame_width * frame_height * sizeof(T) (in our
+     *  case cuComplex) * batch_size.
+     *
+     *  \param[in] rank Dimensionality of the transform.
+     *  \param[in] n Array of size `rank`, describing the size of each dimension, `n[0]` being the size of the outermost
+     *  and `n[rank-1]` innermost (contiguous) dimension of a transform.
+     *  \param[in] inembed Pointer of size `rank` that indicates the storage dimensions of the input data in memory.
+     *  If set to NULL all other advanced data layout parameters are ignored.
+     *  \param[in] istride Indicates the distance between two successive input elements in the least significant
+     *  (i.e., innermost) dimension.
+     *  \param[in] idist Indicates the distance between the first element of two consecutive signals in a batch of the
+     *  input data.
+     *  \param inputtype Type of input data.
+     *  \param[in] onembed Pointer of size `rank` that indicates the storage dimensions of the output data in memory. If
+     *  set to NULL all other advanced data layout parameters are ignored.
+     *  \param[in] ostride Indicates the distance between two successive output elements in the output array in the
+     *  least significant (i.e., innermost) dimension.
+     *  \param[in] odist Indicates the distance between the first element of two consecutive signals in a batch of the
+     *  output data.
+     *  \param[in] outputtype Type of output data.
+     *  \param[in] batch Batch size for this transform.
+     *  \param[in] executiontype Type of data to be used for computations.
      */
     void XtplanMany(int rank,
                     long long* n,
@@ -73,22 +132,24 @@ class CufftHandle
                     long long batch,
                     cudaDataType executiontype);
 
-    /*! \brief Get a reference to the underlying cufftHandle */
+    /*! \brief Get a reference to the underlying cufftHandle.
+     *  \return cufftHandle& A reference to the cufftHandle instance.
+     */
     cufftHandle& get();
 
-    /*! \brief Implicit cast to the underlying cufftHandle */
+    /*! \brief Implicit cast to the underlying cufftHandle. */
     operator cufftHandle&();
 
-    /*! \brief Implicit cast to a ptr to the underlying cufftHandle */
+    /*! \brief Implicit cast to a ptr to the underlying cufftHandle. */
     operator cufftHandle*();
 
   private:
-    /*! \brief The cufftHandle
-     *
-     * We chose a unique_ptr to represent a possibly uninitialized one
+    /*! \brief The cufftHandle.
+     *  We chose a unique_ptr to represent a possibly uninitialized one.
      */
     std::unique_ptr<cufftHandle> val_;
 
+    /*! \brief The CUDA stream used for the cufft API. */
     static cudaStream_t stream_;
 };
 } // namespace holovibes::cuda_tools
