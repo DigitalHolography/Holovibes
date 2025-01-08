5e2c97b807cc4b22e1882a32a04e88614585c54f
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2025-01-08 15:06:09 +0100

    wip(information): added widget file

---
+++ Holovibes/includes/gui/other/gui_info_text_edit.hh
@@ -0,0 +1,25 @@
+/*! \file
+ *
+ * \brief TODO
+ */
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QTimer>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QWidgets/QTextEdit>
+
+namespace holovibes::gui
+{
+/*! \class CurvePlot
+ *
+ * \brief Widget wrapping for a QtChart. Used to display Chart computations.
+ */
+class InfoTextEdit : public QTextEdit
+{
+    QObject
+};
+} // namespace holovibes::gui
