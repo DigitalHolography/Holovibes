7eaf54a0a97db75686940ffba35bf5e0ff9c63ec
ArthurCourselle <courselle.arthur@gmail.com>
2025-01-10 11:19:53 +0100

    fix: cleanup unused tensor_multiply_vector

--- Holovibes/cuda_sources/tools_compute.cu
+++ Holovibes/cuda_sources/tools_compute.cu
@@ -71,28 +71,6 @@ __global__ void kernel_tensor_multiply_vector_nyquist_compensation(float* output
     output[index] = val;
 }
 
-__global__ void kernel_tensor_multiply_vector(float* output,
-                                              const float* tensor,
-                                              const float* vector,
-                                              const size_t frame_res,
-                                              const ushort f_start,
-                                              const ushort f_end)
-{
-    const uint index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (index >= frame_res)
-        return;
-
-    float val = 0.0f;
-    for (uint i = f_start; i <= f_end; i++)
-    {
-        const float* current_frame = tensor + i * frame_res;
-        val += current_frame[index] * vector[i];
-    }
-
-    output[index] = val;
-}
-
 void tensor_multiply_vector_nyquist_compensation(float* output,
                                                  const float* tensor,
                                                  const float* vector,
@@ -115,20 +93,6 @@ void tensor_multiply_vector_nyquist_compensation(float* output,
                                                                                        nyquist_freq,
                                                                                        even,
                                                                                        m1);
-    cudaCheckError();
-}
-
-void tensor_multiply_vector(float* output,
-                            const float* tensor,
-                            const float* vector,
-                            const size_t frame_res,
-                            const ushort f_start,
-                            const ushort f_end,
-                            const cudaStream_t stream)
-{
-    uint threads = get_max_threads_1d();
-    uint blocks = map_blocks_to_problem(frame_res, threads);
-    kernel_tensor_multiply_vector<<<blocks, threads, 0, stream>>>(output, tensor, vector, frame_res, f_start, f_end);
     cudaCheckError();
 }
 
