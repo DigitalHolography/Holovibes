8134a56e3ea1bb0a174df680ab38fde01ab79429
ArthurCourselle <courselle.arthur@gmail.com>
2025-01-10 11:18:05 +0100

    fix: fix wrong parameter comment

--- Holovibes/cuda_includes/tools_compute.cuh
+++ Holovibes/cuda_includes/tools_compute.cuh
@@ -55,7 +55,7 @@ void gpu_normalize(float* const input,
  * \param[in]  vector     The vector
  * \param[in]  frame_res  The resolution of a single frame
  * \param[in]  f_start    The start index
- * \param[in]  f_end      The index of the nyquist frequency
+ * \param[in]  f_end      The end index
  * \param[in]  stream     The cuda stream
  */
 void tensor_multiply_vector(float* output,
@@ -86,7 +86,7 @@ void tensor_multiply_vector(float* output,
  * \param[in]  vector           The vector
  * \param[in]  frame_res        The resolution of a single frame
  * \param[in]  f_start          The start index
- * \param[in]  f_end            The end indexes
+ * \param[in]  f_end            The end index
  * \param[in]  nyquist_freq     The index of the Nyquist frequency in vector
  * \param[in]  even             Boolean true if time transform size is even
  * \param[in]  m1               Boolean true if we are calculating M1
